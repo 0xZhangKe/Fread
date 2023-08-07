14f78d7fedd3e007140601d82b3919f6ecd53be9
zhangke <kezhang404@gmail.com>
2023-08-08 00:01:08 +0800

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation(project(path = ":feature:profile"))
 
     testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(libs.bundles.kotlin)
     implementation(libs.androidx.core)

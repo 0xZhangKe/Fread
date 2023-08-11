8c3e7e172846d5868729e3e1e19d53dca0e33037
zhangke <kezhang404@gmail.com>
2023-08-11 23:55:02 +0800

    adjust AllFeedsManagerScreen UI

--- feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/manager/AllFeedsManagerScreen.kt
+++ feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/manager/AllFeedsManagerScreen.kt
@@ -1,5 +1,6 @@
 package com.zhangke.utopia.feeds.pages.home.manager
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,6 +30,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -83,17 +86,26 @@ private fun AllFeedsManagerScreenContent(
             }
         }
 
+        val columns = 3
+        val horizontalEdgePadding = 10.dp
+        val horizontalSpace = 14.dp
         LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
+            columns = GridCells.Fixed(columns),
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(horizontal = horizontalEdgePadding)
                 .weight(1F),
             horizontalArrangement = Arrangement.Center,
         ) {
-            items(feedsList) { uiState ->
+            items(
+                items = feedsList,
+            ) { uiState ->
                 AllFeedsManagerItem(
-                    modifier = Modifier.fillMaxWidth(),
-                    uiState,
+                    modifier = Modifier
+                        .padding(horizontal = horizontalSpace / 2)
+                        .fillMaxWidth(),
+                    uiState = uiState,
+                    onClick = {}
                 )
             }
         }
@@ -104,17 +116,31 @@ private fun AllFeedsManagerScreenContent(
 private fun AllFeedsManagerItem(
     modifier: Modifier = Modifier,
     uiState: FeedsPageUiState,
+    onClick: () -> Unit,
 ) {
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center,
     ) {
         Card(
+            modifier = Modifier.fillMaxWidth(),
             elevation = CardDefaults.cardElevation(3.dp),
         ) {
-            Column(modifier = Modifier.padding(horizontal = 6.dp, vertical = 4.dp)) {
+            Column(
+                modifier = Modifier
+                    .clickable(onClick = onClick)
+                    .padding(horizontal = 6.dp, vertical = 4.dp)
+            ) {
+                Text(
+                    text = uiState.name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+
                 if (uiState.serverList.isSingle()) {
                     Row(
+                        modifier = Modifier.padding(top = 2.dp),
                         verticalAlignment = Alignment.CenterVertically,
                     ) {
                         val serverInfo = uiState.serverList.first()
@@ -122,7 +148,7 @@ private fun AllFeedsManagerItem(
                         if (serverThumbnail.isNullOrEmpty().not()) {
                             AsyncImage(
                                 modifier = Modifier
-                                    .size(20.dp)
+                                    .size(16.dp)
                                     .clip(CircleShape),
                                 model = uiState.serverList.first().thumbnail,
                                 contentScale = ContentScale.Crop,
@@ -132,27 +158,26 @@ private fun AllFeedsManagerItem(
                         }
                         Text(
                             text = serverInfo.name,
-                            style = MaterialTheme.typography.bodySmall,
+                            style = MaterialTheme.typography.labelSmall,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
                         )
                     }
                 } else {
                     val avatarList = uiState.serverList.mapNotNull { it.thumbnail }.take(5)
                     AvatarHorizontalStack(
+                        modifier = Modifier.padding(top = 2.dp),
                         avatars = avatarList,
-                        avatarSize = 20.dp,
+                        avatarSize = 16.dp,
                     )
                 }
 
                 Text(
                     modifier = Modifier.padding(top = 2.dp),
-                    text = uiState.name,
-                    style = MaterialTheme.typography.bodyLarge,
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 2.dp),
                     text = "Source count: ${uiState.sourceList.size}",
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.labelSmall,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                 )
             }
         }

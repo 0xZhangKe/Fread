7e02d7c2493cc7903b16573fca6d00fefc80fd10
zhangke <kezhang404@gmail.com>
2023-08-23 23:33:27 +0800

    Use AndroidView parse HTML text

--- commonbiz/status-ui/src/main/java/com/zhangke/utopia/status/ui/BlogNode.kt
+++ commonbiz/status-ui/src/main/java/com/zhangke/utopia/status/ui/BlogNode.kt
@@ -1,20 +1,18 @@
 package com.zhangke.utopia.status.ui
 
 import android.widget.TextView
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -93,11 +91,22 @@ fun BlogContentComposable(
             )
         }
         if (blog.content.isEmpty().not()) {
-            Text(
+            AndroidView(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 8.dp, end = 8.dp, top = 6.dp),
-                text = blog.content,
+                    .wrapContentHeight()
+                    .padding(start = 15.dp, end = 15.dp, top = 8.dp),
+                factory = {
+                    TextView(it).apply {
+                        textSize = 14F
+                    }
+                },
+                update = {
+                    it.text = HtmlCompat.fromHtml(
+                        blog.content,
+                        HtmlCompat.FROM_HTML_MODE_COMPACT,
+                    )
+                }
             )
         }
         BlogMedias(

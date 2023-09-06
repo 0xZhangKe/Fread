068620e425489b7fd45be1812e5c54210402ac70
zhangke <kezhang404@gmail.com>
2023-09-07 00:29:47 +0800

    complete InfinityBox

--- framework/src/main/java/com/zhangke/framework/composable/image/viewer/ImageViewer.kt
+++ framework/src/main/java/com/zhangke/framework/composable/image/viewer/ImageViewer.kt
@@ -1,13 +1,11 @@
 package com.zhangke.framework.composable.image.viewer
 
+import android.util.Log
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +18,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.toSize
+import com.zhangke.framework.composable.infinite.InfiniteBox
 import com.zhangke.framework.utils.pxToDp
 import kotlinx.coroutines.launch
 
@@ -34,7 +33,7 @@ fun ImageViewer(
     var latestSize: Size? by remember {
         mutableStateOf(null)
     }
-    Box(
+    InfiniteBox(
         modifier = modifier
             .onGloballyPositioned { position ->
                 val currentSize = position.size.toSize()
@@ -63,11 +62,12 @@ fun ImageViewer(
     ) {
         Box(
             modifier = Modifier
-                .horizontalScroll(rememberScrollState())
-                .verticalScroll(rememberScrollState())
+                .offset(
+                    x = state.currentOffsetXPixel.pxToDp(density),
+                    y = state.currentOffsetYPixel.pxToDp(density)
+                )
                 .width(state.currentWidthPixel.pxToDp(density))
                 .height(state.currentHeightPixel.pxToDp(density))
-                .offset(x = state.currentOffsetXPixel.pxToDp(density), y = state.currentOffsetYPixel.pxToDp(density))
         ) {
             content()
         }
---
+++ framework/src/main/java/com/zhangke/framework/composable/infinite/InfinityBoxState.kt
@@ -0,0 +1,95 @@
+package com.zhangke.framework.composable.infinite
+
+import android.util.Log
+import androidx.compose.animation.core.AnimationScope
+import androidx.compose.animation.core.AnimationState
+import androidx.compose.animation.core.AnimationVector2D
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateDecay
+import androidx.compose.animation.core.exponentialDecay
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.Velocity
+import com.zhangke.framework.composable.Bounds
+import com.zhangke.framework.composable.toOffset
+
+@Composable
+fun rememberInfinityBoxState(): InfinityBoxState {
+    return rememberSaveable(saver = InfinityBoxState.Saver) {
+        InfinityBoxState()
+    }
+}
+
+@Stable
+class InfinityBoxState {
+
+    var exceed: Boolean by mutableStateOf(false)
+
+    private val _currentOffset = mutableStateOf(Offset.Zero)
+    val currentOffset by _currentOffset
+
+    internal var layoutSize = Size.Zero
+
+    internal var draggableBounds = Bounds.EMPTY
+        set(value) {
+            cancelAnimation()
+            field = value
+            _currentOffset.value = Offset.Zero
+            Log.d("U_TEST", "draggableBounds:$draggableBounds, exceed: $exceed")
+        }
+
+    private var flingAnimation: AnimationScope<Offset, AnimationVector2D>? = null
+
+    fun drag(dragAmount: Offset) {
+        cancelAnimation()
+        if (!exceed) return
+        val newOffset = currentOffset + dragAmount
+        _currentOffset.value = draggableBounds.coerceIn(newOffset)
+    }
+
+    suspend fun fling(initialVelocity: Velocity) {
+        if (!exceed) return
+        val initialValue = currentOffset
+        AnimationState(
+            typeConverter = Offset.VectorConverter,
+            initialValue = initialValue,
+            initialVelocity = initialVelocity.toOffset(),
+        ).animateDecay(exponentialDecay()) {
+            flingAnimation = this
+            if (draggableBounds.outsideAbsolute(value) ||
+                velocity.getDistance() <= 300
+            ) {
+                flingAnimation = null
+                cancelAnimation()
+                return@animateDecay
+            }
+            _currentOffset.value = draggableBounds.coerceIn(value)
+        }
+    }
+
+    private fun cancelAnimation() {
+        val animation = flingAnimation ?: return
+        flingAnimation = null
+        if (!animation.isRunning) return
+        animation.cancelAnimation()
+    }
+
+    companion object {
+
+        val Saver: Saver<InfinityBoxState, *> = Saver(
+            save = {
+                emptyArray<Unit>()
+            },
+            restore = {
+                InfinityBoxState()
+            },
+        )
+    }
+}

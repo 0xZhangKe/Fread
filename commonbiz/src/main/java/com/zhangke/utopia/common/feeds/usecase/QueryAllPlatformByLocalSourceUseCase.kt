d518957b445995d9f6d9017a96292d682ce4b95d
zhangke <kezhang404@gmail.com>
2023-11-20 00:11:32 +0800

    add use case

--- commonbiz/src/main/java/com/zhangke/utopia/common/feeds/usecase/QueryAllPlatformByLocalSourceUseCase.kt
+++ commonbiz/src/main/java/com/zhangke/utopia/common/feeds/usecase/QueryAllPlatformByLocalSourceUseCase.kt
@@ -1,11 +1,17 @@
 package com.zhangke.utopia.common.feeds.usecase
 
 import com.zhangke.utopia.common.feeds.repo.FeedsRepo
+import com.zhangke.utopia.status.StatusProvider
+import com.zhangke.utopia.status.platform.BlogPlatform
 import javax.inject.Inject
 
 class QueryAllPlatformByLocalSourceUseCase @Inject constructor(
     private val feedsRepo: FeedsRepo,
+    private val statusProvider: StatusProvider,
 ) {
 
-//    suspend operator fun invoke(): List<Platfor>
+    suspend operator fun invoke(): Result<List<BlogPlatform>> {
+        val allUriList = feedsRepo.queryAll().flatMap { it.sourceUriList }
+        return statusProvider.platformResolver.resolveBySourceUriList(allUriList)
+    }
 }

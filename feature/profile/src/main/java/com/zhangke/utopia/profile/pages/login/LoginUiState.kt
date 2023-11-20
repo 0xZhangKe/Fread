8ceb050feca4e1bdb8a130ae5406b9a10cfa2252
ZhangKe <kezhang404@gmail.com>
2023-11-20 17:38:29 +0800

    update login screen

---
+++ feature/profile/src/main/java/com/zhangke/utopia/profile/pages/login/LoginUiState.kt
@@ -0,0 +1,7 @@
+package com.zhangke.utopia.profile.pages.login
+
+import com.zhangke.utopia.status.platform.BlogPlatform
+
+data class LoginUiState(
+    val platformList: List<BlogPlatform>,
+)
--- feature/profile/src/main/java/com/zhangke/utopia/profile/pages/login/LoginViewModel.kt
+++ feature/profile/src/main/java/com/zhangke/utopia/profile/pages/login/LoginViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.zhangke.utopia.common.feeds.usecase.QueryAllPlatformByLocalSourceUseCase
 import com.zhangke.utopia.status.StatusProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,11 +17,14 @@ class LoginViewModel @Inject constructor(
     private val queryAllPlatform: QueryAllPlatformByLocalSourceUseCase,
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(LoginUiState(emptyList()))
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
+
     init {
         viewModelScope.launch {
             queryAllPlatform()
-                .onSuccess {
-
+                .onSuccess { platformList ->
+                    _uiState.update { it.copy(platformList = platformList) }
                 }.onFailure {
 
                 }

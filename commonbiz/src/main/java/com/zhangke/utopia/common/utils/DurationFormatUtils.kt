80ab7b31780a4fd593bbc0cc1f4623ff2d759ff5
ZhangKe <kezhang404@gmail.com>
2023-11-29 17:58:34 +0800

    add poll options

---
+++ commonbiz/src/main/java/com/zhangke/utopia/common/utils/DurationFormatUtils.kt
@@ -0,0 +1,39 @@
+package com.zhangke.utopia.common.utils
+
+import com.zhangke.framework.utils.appContext
+import com.zhangke.utopia.commonbiz.R
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+
+fun Duration.formattedString(): String {
+    val context = appContext
+    val builder = StringBuilder()
+    var leftDuration = this
+    val weeks = dayToWeek(leftDuration.inWholeDays)
+    if (weeks > 0) {
+        leftDuration -= (weeks * 7L).days
+        builder.append("$weeks${context.getString(R.string.duration_week)}")
+    }
+    val days = leftDuration.inWholeDays
+    if (days > 0) {
+        leftDuration -= days.days
+        builder.append("$days${context.getString(R.string.duration_day)}")
+    }
+    val hours = leftDuration.inWholeHours
+    if (hours > 0) {
+        leftDuration -= hours.hours
+        builder.append("$hours${context.getString(R.string.duration_hour)}")
+    }
+    val minutes = leftDuration.inWholeMinutes
+    if (minutes > 0) {
+        leftDuration -= minutes.minutes
+        builder.append("$minutes${context.getString(R.string.duration_minute)}")
+    }
+    return builder.toString()
+}
+
+fun dayToWeek(day: Long): Long {
+    return day / 7
+}

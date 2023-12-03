610045be211407950c9b4ce78ef4ca2bddfcaf02
zhangke <kezhang404@gmail.com>
2023-12-03 23:43:41 +0800

    sync activity-pub

--- plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/client/CreateActivityPubClientUseCase.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/client/CreateActivityPubClientUseCase.kt
@@ -3,13 +3,12 @@ package com.zhangke.utopia.activitypub.app.internal.client
 import com.google.gson.Gson
 import com.zhangke.activitypub.ActivityPubClient
 import com.zhangke.activitypub.entities.ActivityPubTokenEntity
+import com.zhangke.framework.architect.http.GlobalOkHttpClient
 import com.zhangke.framework.architect.http.newRetrofit
 import com.zhangke.framework.architect.json.globalGson
 import com.zhangke.utopia.activitypub.app.internal.utils.ActivityPubUrl
 import retrofit2.Retrofit
 import javax.inject.Inject
-
-private const val REDIRECT_URI = "utopia://oauth.utopia"
 
 class CreateActivityPubClientUseCase @Inject constructor(
     private val obtainActivityPubApplicationUseCase: ObtainActivityPubApplicationUseCase,
@@ -17,18 +16,14 @@ class CreateActivityPubClientUseCase @Inject constructor(
 
     suspend operator fun invoke(
         host: String,
-        retrofit: Retrofit = newRetrofit(ActivityPubUrl.create(host)!!.completenessUrl),
         gson: Gson = globalGson,
-        redirectUrl: String = REDIRECT_URI,
         tokenProvider: suspend () -> ActivityPubTokenEntity?,
-        onAuthorizeFailed: suspend (url: String, client: ActivityPubClient) -> Unit,
+        onAuthorizeFailed: suspend () -> Unit,
     ): ActivityPubClient {
-        val application = obtainActivityPubApplicationUseCase(host)
         return ActivityPubClient(
-            application = application,
-            retrofit = retrofit,
+            baseUrl = "https:$host",
+            httpClient = GlobalOkHttpClient.client,
             gson = gson,
-            redirectUrl = redirectUrl,
             tokenProvider = tokenProvider,
             onAuthorizeFailed = onAuthorizeFailed,
         )

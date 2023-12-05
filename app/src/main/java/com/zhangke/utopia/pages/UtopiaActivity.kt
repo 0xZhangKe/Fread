3b836c717a2e102f8ff1d17dfcdce125d37c309c
zhangke <kezhang404@gmail.com>
2023-12-05 23:37:32 +0800

    fix ui bugs and upgrade voyager version

--- app/src/main/java/com/zhangke/utopia/pages/UtopiaActivity.kt
+++ app/src/main/java/com/zhangke/utopia/pages/UtopiaActivity.kt
@@ -3,8 +3,10 @@ package com.zhangke.utopia.pages
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import cafe.adriel.voyager.navigator.Navigator
@@ -20,13 +22,13 @@ class UtopiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
 
-//        setContentView(createContentView())
         setContent {
             UtopiaTheme {
                 TransparentNavigator {
                     BottomSheetNavigator(
+                        modifier = Modifier.navigationBarsPadding(),
                         sheetShape = RoundedCornerShape(12.dp),
                     ) {
                         Navigator(UtopiaScreen())
@@ -35,98 +37,4 @@ class UtopiaActivity : AppCompatActivity() {
             }
         }
     }
-
-//    private fun createContentView(): View {
-//        val container = LinearLayout(this).apply {
-//            layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT
-//            )
-//            orientation = LinearLayout.VERTICAL
-//            gravity = Gravity.CENTER_VERTICAL
-//        }
-//
-//        container.addView(
-//            createInlineItemView(
-////                "https://video.twimg.com/ext_tw_video/1712110948700352512/pu/vid/avc1/720x1280/i43wruptl2R9KHAZ.mp4?tag=12",
-//                "https://video.twimg.com/ext_tw_video/1718975693181100033/pu/vid/avc1/480x852/QlSJulSIWV3MnXV_.mp4?tag=12",
-//                true
-//            )
-//        )
-//
-//        container.addView(View(this).apply {
-//            layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                6
-//            )
-//            setBackgroundColor(Color.BLUE)
-//        })
-//
-//        container.addView(View(this).apply {
-//            layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                300
-//            )
-//        })
-//
-//        container.addView(
-//            createInlineItemView(
-//                "https://video.twimg.com/ext_tw_video/1712110948700352512/pu/vid/avc1/720x1280/i43wruptl2R9KHAZ.mp4?tag=12",
-//                true
-//            )
-//        )
-//        return container
-//    }
-//
-//    private fun createInlineItemView(
-//        uri: String,
-//        playWhenReady: Boolean,
-//    ): View {
-//        return FrameLayout(this).apply {
-//            layoutParams = ViewGroup.MarginLayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//            ).apply {
-//                marginStart = 40
-//                marginEnd = 40
-//                topMargin = 100
-//                bottomMargin = 20
-//            }
-//            addView(createInlineVideoView(uri, playWhenReady))
-//        }
-//    }
-
-//    @androidx.annotation.OptIn(UnstableApi::class)
-//    private fun createInlineVideoView(
-//        uri: String,
-//        playWhenReady: Boolean,
-//    ): View {
-////        Log.d("U_TEST", "createInlineVideoView($playWhenReady)")
-//        val exoPlayer = ExoPlayer.Builder(this)
-//            .build()
-//            .apply {
-//                val defaultDataSourceFactory = DefaultDataSource.Factory(this@UtopiaActivity)
-//                val dataSourceFactory =
-//                    DefaultDataSource.Factory(this@UtopiaActivity, defaultDataSourceFactory)
-//                val source = ProgressiveMediaSource.Factory(dataSourceFactory)
-//                    .createMediaSource(MediaItem.fromUri(uri))
-//                setMediaSource(source)
-//                prepare()
-//                Log.d("U_TEST", "source:$source, player:$this")
-//                this.playWhenReady = playWhenReady
-//                videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
-//                repeatMode = Player.REPEAT_MODE_OFF
-//            }
-//        return SurfaceView(this).apply {
-//            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-//            useController = false
-//            controllerAutoShow = false
-//            player = exoPlayer
-//            exoPlayer.setVideoSurfaceView(this)
-//            layoutParams = FrameLayout.LayoutParams(
-//                FrameLayout.LayoutParams.MATCH_PARENT,
-//                400,
-//            )
-//        }
-//    }
 }
--- feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/FeedsHomeScreenContent.kt
+++ feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/FeedsHomeScreenContent.kt
@@ -1,6 +1,8 @@
 package com.zhangke.utopia.feeds.pages.home
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -22,6 +25,7 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,6 +39,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,28 +72,37 @@ internal fun FeedsHomeScreenContent(
     var topBarItems: List<BlogPlatform> by rememberSaveable {
         mutableStateOf(emptyList())
     }
-    val selectedIndex = uiState.tabIndex
-    LoadableLayout(
-        modifier = Modifier.fillMaxSize(),
-        state = uiState.pageUiStateList,
-    ) { feedsList ->
-        Scaffold(
-            snackbarHost = {
-                SnackbarHost(hostState = snackbarHostState)
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = onPostStatusClick,
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "Add",
-                    )
-                }
+    val density = LocalDensity.current
+
+    Scaffold(
+        modifier = Modifier.navigationBarsPadding(),
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onPostStatusClick,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                )
             }
-        ) { paddings ->
+        },
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.also {
+            Log.d("U_TEST", "${it.getTop(density)}, ${it.getBottom(density)}")
+        },
+    ) { paddings ->
+        Log.d("U_TEST", "paddings top:${paddings.calculateTopPadding()}, bottom:${paddings.calculateBottomPadding()}")
+        val selectedIndex = uiState.tabIndex
+        LoadableLayout(
+            modifier = Modifier
+                .padding(paddings)
+                .fillMaxSize(),
+            state = uiState.pageUiStateList,
+        ) { feedsList ->
             TopOutTopBarLayout(
-                modifier = Modifier.padding(paddings),
+                modifier = Modifier.fillMaxSize(),
                 topBar = {
                     if (topBarItems.isNotEmpty()) {
                         FeedsHomeTopBar(

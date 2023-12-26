a45c285ce6897b6ce00b74b7d56380231b1c00c5
ZhangKe <kezhang404@gmail.com>
2023-12-26 11:21:41 +0800

    add web finger repo

--- plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/usecase/baseurl/GetBaseUrlFromWebFingerUseCase.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/usecase/baseurl/GetBaseUrlFromWebFingerUseCase.kt
@@ -1,18 +1,27 @@
 package com.zhangke.utopia.activitypub.app.internal.usecase.baseurl
 
 import com.zhangke.framework.utils.WebFinger
-import com.zhangke.utopia.activitypub.app.internal.db.ActivityPubDatabases
+import com.zhangke.utopia.activitypub.app.internal.auth.ActivityPubClientManager
+import com.zhangke.utopia.activitypub.app.internal.repo.WebFingerToBaseUrlRepo
 import javax.inject.Inject
 
 class GetBaseUrlFromWebFingerUseCase @Inject constructor(
-    private val activityPubDatabases: ActivityPubDatabases,
+    private val activityPubRepo: WebFingerToBaseUrlRepo,
+    private val clientManager: ActivityPubClientManager,
     private val getBasicCommonBaseUrl: GetBasicCommonBaseUrlUseCase,
 ) {
 
-    suspend operator fun invoke(webFinger: WebFinger): String {
-        activityPubDatabases.getWebFingerToBaseUrlDao().queryBaseUrl(webFinger)?.let {
-            return it
-        }
-
+    suspend operator fun invoke(webFinger: WebFinger): Result<String?> {
+        activityPubRepo.queryBaseUrl(webFinger)
+            ?.let { return Result.success(it) }
+        return clientManager.getClient(getBasicCommonBaseUrl())
+            .accountRepo
+            .lookup(webFinger.toString())
+            .map { it?.url }
+            .onSuccess {
+                if (it.isNullOrEmpty().not()) {
+                    activityPubRepo.insert(webFinger, it!!)
+                }
+            }
     }
 }

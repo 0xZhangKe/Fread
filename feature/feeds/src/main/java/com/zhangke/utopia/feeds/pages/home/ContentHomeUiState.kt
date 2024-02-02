c9cbb58e99a258a6e763119f916b53b4cd1396f6
ZhangKe <kezhang404@gmail.com>
2024-02-02 17:55:19 +0800

    complete post status interaction

--- feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/ContentHomeUiState.kt
+++ feature/feeds/src/main/java/com/zhangke/utopia/feeds/pages/home/ContentHomeUiState.kt
@@ -1,10 +1,12 @@
 package com.zhangke.utopia.feeds.pages.home
 
+import com.zhangke.utopia.status.account.LoggedAccount
 import com.zhangke.utopia.status.model.ContentConfig
 
 data class ContentHomeUiState(
     val currentPageIndex: Int,
     val contentConfigList: List<ContentConfig>,
+    val accountList: List<LoggedAccount>,
 ) {
 
     val currentConfig: ContentConfig? get() = contentConfigList.getOrNull(currentPageIndex)

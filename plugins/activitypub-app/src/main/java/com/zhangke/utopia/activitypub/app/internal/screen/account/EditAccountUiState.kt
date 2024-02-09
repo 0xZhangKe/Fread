2f390bb3626a17b22d00287dd424ac1465c16e91
ZhangKe <kezhang404@gmail.com>
2024-02-09 22:27:00 +0800

    add edit account info screen

---
+++ plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/screen/account/EditAccountUiState.kt
@@ -0,0 +1,17 @@
+package com.zhangke.utopia.activitypub.app.internal.screen.account
+
+data class EditAccountUiState(
+    val name: String,
+    val editName: String,
+    val banner: String,
+    val editBanner: String,
+    val avatar: String,
+    val editAvatar: String,
+    val description: String,
+    val editDescription: String,
+)
+
+//data class AccountAboutUiState(
+//    val name: String,
+//    val
+//)

5d5ae9bfa087374edb0b59aa373631dad1635a30
ZhangKe <kezhang404@gmail.com>
2024-02-14 23:15:13 +0800

    try to replace the richtext implement to DaliyTag

--- framework/src/main/java/com/zhangke/framework/composable/text/RichText.kt
+++ framework/src/main/java/com/zhangke/framework/composable/text/RichText.kt
@@ -2,11 +2,24 @@ package com.zhangke.framework.composable.text
 
 import android.view.Gravity
 import android.widget.TextView
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.dmytroshuba.dailytags.core.simple.SimpleMarkupParser
+import com.dmytroshuba.dailytags.core.simple.render
+import com.dmytroshuba.dailytags.markdown.rules.HtmlRules
+import com.dmytroshuba.dailytags.markdown.rules.MarkdownRules
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun RichText(
@@ -17,6 +30,23 @@ fun RichText(
     maxLines: Int = Int.MAX_VALUE,
     minLines: Int = 1,
 ) {
+//    var renderedText: AnnotatedString by remember {
+//        mutableStateOf(AnnotatedString(""))
+//    }
+//    LaunchedEffect(text) {
+//        withContext(Dispatchers.IO) {
+//            renderedText = try {
+//                val rules = MarkdownRules.toList() + HtmlRules.toList()
+//                val parser = SimpleMarkupParser()
+//                parser.parse(text, rules)
+//                    .render()
+//                    .toAnnotatedString()
+//            } catch (e: Throwable) {
+//                AnnotatedString(text)
+//            }
+//        }
+//    }
+//    Text(text = renderedText)
     AndroidView(
         modifier = modifier,
         factory = {

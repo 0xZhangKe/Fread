2557b7b9336ccea691ade99554e97384f74ed619
zhangke <kezhang404@gmail.com>
2024-03-31 23:16:27 +0800

    add accountUri for VM

--- feature/explore/src/main/java/com/zhangke/utopia/explore/screens/home/tab/ExplorerFeedsContainerViewModel.kt
+++ feature/explore/src/main/java/com/zhangke/utopia/explore/screens/home/tab/ExplorerFeedsContainerViewModel.kt
@@ -1,32 +1,37 @@
 package com.zhangke.utopia.explore.screens.home.tab
 
 import com.zhangke.framework.lifecycle.ContainerViewModel
-import com.zhangke.utopia.common.status.usecase.BuildStatusUiStateUseCase
 import com.zhangke.utopia.commonbiz.shared.usecase.InteractiveHandler
+import com.zhangke.utopia.explore.usecase.GetExplorerItemUseCase
 import com.zhangke.utopia.status.StatusProvider
+import com.zhangke.utopia.status.uri.FormalUri
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
 class ExplorerFeedsContainerViewModel(
     private val statusProvider: StatusProvider,
     private val interactiveHandler: InteractiveHandler,
-    private val buildStatusUiState: BuildStatusUiStateUseCase,
+    private val getExplorerItem: GetExplorerItemUseCase,
 ) : ContainerViewModel<ExplorerFeedsViewModel, ExplorerFeedsContainerViewModel.Params>() {
 
     override fun createSubViewModel(params: Params): ExplorerFeedsViewModel {
         return ExplorerFeedsViewModel(
             type = params.type,
+            accountUri = params.accountUri,
             statusProvider = statusProvider,
             interactiveHandler = interactiveHandler,
-            buildStatusUiState = buildStatusUiState,
+            getExplorerItem = getExplorerItem,
         )
     }
 
-    fun getSubViewModel(type: ExplorerFeedsTabType): ExplorerFeedsViewModel {
-        return obtainSubViewModel(Params(type))
+    fun getSubViewModel(type: ExplorerFeedsTabType, accountUri: FormalUri): ExplorerFeedsViewModel {
+        return obtainSubViewModel(Params(type, accountUri))
     }
 
-    class Params(val type: ExplorerFeedsTabType) : SubViewModelParams() {
+    class Params(
+        val type: ExplorerFeedsTabType,
+        val accountUri: FormalUri,
+    ) : SubViewModelParams() {
 
         override val key: String
             get() = type.name

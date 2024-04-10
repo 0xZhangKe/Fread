198e120169d5443e04380be125026b18748d8031
zhangke <kezhang404@gmail.com>
2024-04-10 22:56:36 +0800

    complete search bar

--- feature/explore/src/main/java/com/zhangke/utopia/explore/screens/home/ExplorerHomeScreen.kt
+++ feature/explore/src/main/java/com/zhangke/utopia/explore/screens/home/ExplorerHomeScreen.kt
@@ -1,47 +1,25 @@
 package com.zhangke.utopia.explore.screens.home
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getViewModel
 import com.zhangke.framework.composable.HorizontalPagerWithTab
 import com.zhangke.framework.composable.LocalSnackbarHostState
-import com.zhangke.framework.composable.SimpleIconButton
 import com.zhangke.framework.composable.rememberSnackbarHostState
-import com.zhangke.utopia.explore.R
 import com.zhangke.utopia.explore.screens.home.tab.ExplorerFeedsTab
 import com.zhangke.utopia.explore.screens.home.tab.ExplorerFeedsTabType
 import com.zhangke.utopia.explore.screens.search.bar.ExplorerSearchBar
 import com.zhangke.utopia.status.account.LoggedAccount
-import com.zhangke.utopia.status.ui.BlogAuthorAvatar
 
 class ExplorerHomeScreen : Screen {
 
@@ -72,15 +50,11 @@ class ExplorerHomeScreen : Screen {
                     .fillMaxSize()
             ) {
                 val selectedAccount = uiState.selectedAccount
-                if (uiState.loggedAccountsList.size < 2 || selectedAccount == null) {
-                    ExplorerSearchBar()
-                } else {
-                    SelectAccountTopBar(
-                        account = uiState.selectedAccount,
-                        accountList = uiState.loggedAccountsList,
-                        onAccountSelected = onAccountSelected,
-                    )
-                }
+                ExplorerSearchBar(
+                    selectedAccount = uiState.selectedAccount,
+                    accountList = uiState.loggedAccountsList,
+                    onAccountSelected = onAccountSelected,
+                )
                 if (selectedAccount != null) {
                     CompositionLocalProvider(
                         LocalSnackbarHostState provides snackbarHostState
@@ -103,98 +77,6 @@ class ExplorerHomeScreen : Screen {
                         }
                         HorizontalPagerWithTab(
                             tabList = tabs,
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun SelectableAccountTopBar(
-        account: LoggedAccount,
-        accountList: List<LoggedAccount>,
-        onAccountSelected: (LoggedAccount) -> Unit,
-    ) {
-
-    }
-
-    @Composable
-    private fun SelectAccountTopBar(
-        account: LoggedAccount,
-        accountList: List<LoggedAccount>,
-        onAccountSelected: (LoggedAccount) -> Unit,
-    ) {
-        var selectAccountPopupExpanded by remember {
-            mutableStateOf(false)
-        }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = stringResource(R.string.explorer_tab_title),
-                style = MaterialTheme.typography.titleLarge,
-            )
-            Spacer(modifier = Modifier.weight(1F))
-            SimpleIconButton(
-                onClick = {},
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search",
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Box {
-                Row(
-                    modifier = Modifier.clickable {
-                        selectAccountPopupExpanded = !selectAccountPopupExpanded
-                    },
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    BlogAuthorAvatar(
-                        modifier = Modifier.size(32.dp),
-                        imageUrl = account.avatar,
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = account.userName,
-                        style = MaterialTheme.typography.bodyLarge,
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    SimpleIconButton(
-                        onClick = {
-                            selectAccountPopupExpanded = !selectAccountPopupExpanded
-                        },
-                        imageVector = Icons.Default.ArrowDropDown,
-                        contentDescription = "Select Account",
-                    )
-                }
-                DropdownMenu(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    expanded = selectAccountPopupExpanded,
-                    onDismissRequest = { selectAccountPopupExpanded = false },
-                ) {
-                    accountList.forEach {
-                        DropdownMenuItem(
-                            text = {
-                                Row {
-                                    Text(
-                                        modifier = Modifier.alignByBaseline(),
-                                        text = it.userName,
-                                    )
-                                    Text(
-                                        modifier = Modifier
-                                            .padding(start = 2.dp)
-                                            .alignByBaseline(),
-                                        text = "@${it.webFinger.host}",
-                                        style = MaterialTheme.typography.labelSmall,
-                                    )
-                                }
-                            },
-                            onClick = {
-                                selectAccountPopupExpanded = false
-                                onAccountSelected(it)
-                            },
                         )
                     }
                 }

42060f8f3e2743dce485656d88f4c300c4f95d4f
zhangke <kezhang404@gmail.com>
2024-05-18 14:51:25 +0800

    optimize build script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
             )
         }
         debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"

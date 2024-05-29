dcf95348ce75d6342ee30a4f6d87c7f530a0cdc4
zhangke <kezhang404@gmail.com>
2024-05-29 22:31:00 +0800

    check status is fracture

---
+++ plugins/activitypub-app/src/main/java/com/zhangke/utopia/activitypub/app/internal/db/status/ActivityPubStatusDatabases.kt
@@ -0,0 +1,127 @@
+package com.zhangke.utopia.activitypub.app.internal.db.status
+
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.zhangke.utopia.activitypub.app.internal.db.converter.ActivityPubStatusEntityConverter
+import com.zhangke.utopia.activitypub.app.internal.db.converter.ActivityPubStatusSourceTypeConverter
+import com.zhangke.utopia.activitypub.app.internal.db.converter.FormalBaseUrlConverter
+import com.zhangke.utopia.activitypub.app.internal.model.ActivityPubStatusSourceType
+import com.zhangke.utopia.common.status.repo.db.converts.IdentityRoleConverter
+import com.zhangke.utopia.common.status.repo.db.converts.StatusConverter
+import com.zhangke.utopia.status.model.IdentityRole
+import com.zhangke.utopia.status.status.model.Status
+
+private const val DB_NAME = "activity_pub_status.db"
+private const val DB_VERSION = 1
+private const val TABLE_NAME = "activity_pub_status"
+
+@Entity(tableName = TABLE_NAME, primaryKeys = ["id", "role", "type", "listId"])
+data class ActivityPubStatusTableEntity(
+    // Status id
+    val id: String,
+    val role: IdentityRole,
+    val type: ActivityPubStatusSourceType,
+    val listId: String?,
+    val status: Status,
+    val createTimestamp: Long,
+    val fracture: Boolean,
+)
+
+@Dao
+interface ActivityPubStatusDao {
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE id = :id AND role = :role AND type = :type")
+    suspend fun query(role: IdentityRole, type: ActivityPubStatusSourceType, id: String): ActivityPubStatusTableEntity?
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE id = :id AND role = :role AND type = :type AND listId = :listId")
+    suspend fun queryStatusInList(role: IdentityRole, type: ActivityPubStatusSourceType, listId: String, id: String): ActivityPubStatusTableEntity?
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE role = :role AND type = :type ORDER BY createTimestamp DESC LIMIT :limit")
+    suspend fun queryTimelineStatus(
+        role: IdentityRole,
+        type: ActivityPubStatusSourceType,
+        limit: Int,
+    ): List<ActivityPubStatusTableEntity>
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE role = :role AND type = :type ORDER BY createTimestamp DESC LIMIT 1")
+    suspend fun queryRecentStatus(
+        role: IdentityRole,
+        type: ActivityPubStatusSourceType
+    ): ActivityPubStatusTableEntity?
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE role = :role AND type = :type AND listId = :listId ORDER BY createTimestamp DESC LIMIT :limit")
+    suspend fun queryListStatus(
+        role: IdentityRole,
+        type: ActivityPubStatusSourceType,
+        listId: String,
+        limit: Int
+    ): List<ActivityPubStatusTableEntity>
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE role = :role AND type = :type AND listId = :listId ORDER BY createTimestamp DESC LIMIT 1")
+    suspend fun queryRecentListStatus(
+        role: IdentityRole,
+        type: ActivityPubStatusSourceType,
+        listId: String,
+    ): ActivityPubStatusTableEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entity: ActivityPubStatusTableEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entities: List<ActivityPubStatusTableEntity>)
+
+    @Query("DELETE FROM $TABLE_NAME WHERE role=:role AND type=:type")
+    suspend fun deleteStatus(role: IdentityRole, type: ActivityPubStatusSourceType)
+
+    @Query("DELETE FROM $TABLE_NAME WHERE id=:id")
+    suspend fun delete(id: String)
+}
+
+@TypeConverters(
+    FormalBaseUrlConverter::class,
+    ActivityPubStatusSourceTypeConverter::class,
+    ActivityPubStatusEntityConverter::class,
+    StatusConverter::class,
+    IdentityRoleConverter::class,
+)
+@Database(
+    entities = [ActivityPubStatusTableEntity::class],
+    version = DB_VERSION,
+    exportSchema = false,
+)
+abstract class ActivityPubStatusDatabases : RoomDatabase() {
+
+    abstract fun getDao(): ActivityPubStatusDao
+
+    companion object {
+
+        private var instance: ActivityPubStatusDatabases? = null
+
+        fun getInstance(context: Context): ActivityPubStatusDatabases {
+            if (instance == null) {
+                synchronized(ActivityPubStatusDatabases::class.java) {
+                    if (instance == null) {
+                        instance = createDatabase(context)
+                    }
+                }
+            }
+            return instance!!
+        }
+
+        private fun createDatabase(context: Context): ActivityPubStatusDatabases {
+            return Room.databaseBuilder(
+                context,
+                ActivityPubStatusDatabases::class.java,
+                DB_NAME
+            ).build()
+        }
+    }
+}

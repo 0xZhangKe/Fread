b80f49542dd4a979b0051f2bc8d1956251921aa2
zhangke <kezhang404@gmail.com>
2024-06-13 23:21:20 +0800

    optimize main drawer icon

--- app/src/main/java/com/zhangke/utopia/screen/main/drawer/MainDrawer.kt
+++ app/src/main/java/com/zhangke/utopia/screen/main/drawer/MainDrawer.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -29,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -189,9 +190,20 @@ private fun ContentConfigItem(
         )
         Spacer(modifier = Modifier.weight(1F))
         SimpleIconButton(
-            modifier = Modifier.align(Alignment.CenterVertically),
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(24.dp),
             onClick = { onEditClick(contentConfig) },
-            imageVector = Icons.Default.Edit,
+            painter = painterResource(com.zhangke.utopia.statusui.R.drawable.ic_mode_edit),
+            contentDescription = "Edit Content Config",
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        SimpleIconButton(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(24.dp),
+            onClick = { onEditClick(contentConfig) },
+            painter = painterResource(com.zhangke.utopia.statusui.R.drawable.ic_drag_indicator),
             contentDescription = "Edit Content Config",
         )
     }
--- framework/src/main/java/com/zhangke/framework/composable/IconButton.kt
+++ framework/src/main/java/com/zhangke/framework/composable/IconButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
@@ -34,6 +35,34 @@ fun SimpleIconButton(
         Icon(
             modifier = iconModifier,
             imageVector = imageVector,
+            contentDescription = contentDescription,
+            tint = tint,
+        )
+    }
+}
+
+@Composable
+fun SimpleIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    painter: Painter,
+    contentDescription: String?,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    tint: Color = LocalContentColor.current,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = colors,
+        interactionSource = interactionSource,
+    ) {
+        Icon(
+            modifier = iconModifier,
+            painter = painter,
             contentDescription = contentDescription,
             tint = tint,
         )

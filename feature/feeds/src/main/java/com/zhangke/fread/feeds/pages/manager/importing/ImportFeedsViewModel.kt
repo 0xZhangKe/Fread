56fc06cd99575d74ba92a5073357d6e90ec308f4
zhangke <kezhang404@gmail.com>
2024-07-07 22:04:07 +0800

    optimize import process

--- feature/feeds/src/main/java/com/zhangke/fread/feeds/pages/manager/importing/ImportFeedsViewModel.kt
+++ feature/feeds/src/main/java/com/zhangke/fread/feeds/pages/manager/importing/ImportFeedsViewModel.kt
@@ -3,16 +3,15 @@ package com.zhangke.fread.feeds.pages.manager.importing
 import android.content.Context
 import android.net.Uri
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
+import com.zhangke.framework.collections.container
 import com.zhangke.framework.collections.remove
 import com.zhangke.framework.ktx.launchInScreenModel
+import com.zhangke.framework.network.SimpleUri
 import com.zhangke.framework.opml.OpmlOutline
 import com.zhangke.framework.opml.OpmlParser
 import com.zhangke.fread.common.status.repo.ContentConfigRepo
 import com.zhangke.fread.status.StatusProvider
 import com.zhangke.fread.status.model.ContentConfig
-import com.zhangke.fread.status.uri.FormalUri
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
@@ -22,6 +21,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -39,110 +39,167 @@ class ImportFeedsViewModel @Inject constructor(
     private var importingJob: Job? = null
 
     fun onFileSelected(uri: Uri) {
-        _uiState.value = _uiState.value.copy(selectedFileUri = uri)
+        _uiState.value = _uiState.value.copy(
+            selectedFileUri = uri,
+            sourceList = emptyList(),
+        )
     }
 
     fun onImportClick(context: Context) {
-        _uiState.update {
-            it.copy(importType = ImportType.IMPORTING)
-        }
+        if (importingJob?.isActive == true) return
+        if (uiState.value.sourceList.isNotEmpty()) return
         importingJob = launchInScreenModel {
-            appendOutputLog("Start parsing outlines...")
-            val list = parseOpml(context, uiState.value.selectedFileUri!!)
-            appendOutputLog("Parsed ${list.size} outlines.")
-            list.forEach {
-                parseOutlineToContent(it)
+            val sourceGroup = parseOpmlToGroup(context, uiState.value.selectedFileUri!!)
+            _uiState.update { it.copy(sourceList = sourceGroup) }
+            sourceGroup.forEach {
+                importGroup(it)
             }
-            appendOutputLog("All outlines parsed.")
-            _uiState.update { it.copy(importType = ImportType.SUCCESS) }
         }
-        importingJob?.invokeOnCompletion { t ->
-            t?.let {
-                _uiState.update {
-                    it.copy(
-                        outputInfoList = it.outputInfoList + ImportOutputLog(
-                            log = t.message ?: "Unknown error",
-                            type = ImportOutputLog.Type.ERROR,
-                        ),
-                        importType = ImportType.FAILED,
-                    )
+    }
+
+    private suspend fun importGroup(group: ImportSourceGroup) {
+        if (checkGroupDeleted(group)) return
+        supervisorScope {
+            group.children.map { source ->
+                async {
+                    importSource(group, source)
                 }
-            }
+            }.awaitAll()
         }
     }
 
-    fun onImportCancelClick() {
-        importingJob?.cancel()
-        appendOutputLog("Cancelled.")
-        _uiState.update { it.copy(importType = ImportType.IDLE) }
+    private suspend fun importSource(group: ImportSourceGroup, source: ImportingSource) {
+        if (checkSourceDeleted(group, source)) return
+        if (source !is ImportingSource.Pending && source !is ImportingSource.Failure) return
+        updateSourceUiState(group, ImportingSource.Importing(source.title, source.url))
+        statusProvider.statusSourceResolver.resolveRssSource(source.url)
+            .onSuccess {
+                updateSourceUiState(
+                    group,
+                    ImportingSource.Success(source.title, source.url, it.uri)
+                )
+            }.onFailure {
+                updateSourceUiState(
+                    group,
+                    ImportingSource.Failure(source.title, source.url, it.message ?: "Unknown error")
+                )
+            }
     }
 
-    fun onImportDialogConfirmClick() {
-        _uiState.update { it.copy(importType = ImportType.IDLE) }
-    }
-
-    fun onContentConfigDelete(contentConfig: ContentConfig) {
+    private fun updateSourceUiState(group: ImportSourceGroup, source: ImportingSource) {
         _uiState.update { state ->
             state.copy(
-                parsedContent = state.parsedContent.remove { it == contentConfig }
+                sourceList = state.sourceList.map { item ->
+                    if (item.title == group.title) {
+                        item.copy(
+                            children = item.children.map { child ->
+                                if (child.url == source.url) {
+                                    source
+                                } else {
+                                    child
+                                }
+                            })
+                    } else {
+                        item
+                    }
+                }
+            )
+        }
+    }
+
+    fun onGroupDelete(group: ImportSourceGroup) {
+        _uiState.update { state ->
+            state.copy(
+                sourceList = state.sourceList.remove { it == group }
+            )
+        }
+    }
+
+    fun onSourceDelete(group: ImportSourceGroup, source: ImportingSource) {
+        _uiState.update { state ->
+            state.copy(
+                sourceList = state.sourceList.map { item ->
+                    if (item == group) {
+                        item.copy(children = item.children.remove { it == source })
+                    } else {
+                        item
+                    }
+                }
             )
         }
     }
 
     fun onSaveClick() {
+        if (importingJob?.isActive == true) return
         launchInScreenModel {
-            configRepo.insert(uiState.value.parsedContent)
+            val mixedContentList = _uiState.value
+                .sourceList
+                .mapNotNull { it.toMixedContent() }
+            if (mixedContentList.isEmpty()) return@launchInScreenModel
+            configRepo.insert(mixedContentList)
             _saveSuccessFlow.emit(Unit)
         }
     }
 
-    private suspend fun parseOutlineToContent(outline: OpmlOutline) {
-        appendOutputLog("Start parsing ${outline.title}...")
-        if (configRepo.checkNameExist(outline.title)){
-            appendOutputLog("${outline.title} exist, ignore!")
-            return
-        }
-        val sourceUriList = mutableListOf<FormalUri>()
-        outline.children
-            .distinctBy { it.xmlUrl }
-            .map { item ->
-                screenModelScope.async {
-                    appendOutputLog("fetching:${item.xmlUrl}")
-                    statusProvider.statusSourceResolver
-                        .resolveRssSource(item.xmlUrl)
-                        .onSuccess {
-                            sourceUriList += it.uri
-                            appendOutputLog("checkout success: ${item.xmlUrl}")
-                        }.onFailure {
-                            appendOutputLog("checkout ${item.xmlUrl} failed: ${it.message}.")
-                        }
-                }
-            }.awaitAll()
-        val contentConfig = ContentConfig.MixedContent(
-            id = 0,
-            order = configRepo.generateNextOrder(),
-            name = outline.title,
-            sourceUriList = sourceUriList,
-        )
-        appendOutputLog("${outline.title} parsed successfully.")
-        _uiState.update {
-            it.copy(parsedContent = it.parsedContent + contentConfig)
+    fun retryImportClick(group: ImportSourceGroup, source: ImportingSource) {
+        launchInScreenModel {
+            importSource(group, source)
         }
     }
 
-    private fun appendOutputLog(
-        log: String,
-        type:
-        ImportOutputLog.Type = ImportOutputLog.Type.NORMAL,
-    ) {
-        _uiState.update {
-            it.copy(
-                outputInfoList = it.outputInfoList + ImportOutputLog(
-                    log = log,
-                    type = type,
+    private fun checkGroupDeleted(group: ImportSourceGroup): Boolean {
+        val currentList = _uiState.value.sourceList
+        return !currentList.container { it.title == group.title }
+    }
+
+    private fun checkSourceDeleted(group: ImportSourceGroup, source: ImportingSource): Boolean {
+        val currentList = _uiState.value.sourceList
+        val existGroup = currentList.firstOrNull { it.title == group.title } ?: return true
+        return !existGroup.children.container { it.url == source.url }
+    }
+
+    private suspend fun ImportSourceGroup.toMixedContent(): ContentConfig.MixedContent? {
+        val successChildren = this.children.filterIsInstance<ImportingSource.Success>()
+        if (successChildren.isEmpty()) return null
+        return ContentConfig.MixedContent(
+            id = 0,
+            order = configRepo.generateNextOrder(),
+            name = this.title,
+            sourceUriList = successChildren.map { it.formalUri },
+        )
+    }
+
+    private suspend fun parseOpmlToGroup(context: Context, uri: Uri): List<ImportSourceGroup> {
+        return parseOpml(context, uri).groupBy { it.title.ifEmpty { "Unknown" } }
+            .map { entry ->
+                ImportSourceGroup(
+                    title = entry.key,
+                    children = entry.value.flatMap { it.allChildren() }.map { it.toSource() }
                 )
-            )
+            }
+    }
+
+    private fun OpmlOutline.toSource(): ImportingSource {
+        return ImportingSource.Pending(
+            title = title.ifEmpty { convertSimpleNameFromUrl(xmlUrl) },
+            url = xmlUrl
+        )
+    }
+
+    private fun convertSimpleNameFromUrl(url: String): String {
+        if (url.isBlank()) return "Unknown"
+        return SimpleUri.parse(url)?.host ?: "Unknown"
+    }
+
+    private fun OpmlOutline.allChildren(): List<OpmlOutline> {
+        val list = mutableListOf<OpmlOutline>()
+        if (xmlUrl.isNotBlank()) {
+            list.add(this)
         }
+        children.forEach {
+            list.addAll(it.allChildren())
+        }
+        return list
     }
 
     private suspend fun parseOpml(

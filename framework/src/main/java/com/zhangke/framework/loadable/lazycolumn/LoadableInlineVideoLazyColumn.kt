e9ff575be119d0c4861e764b609a35a6c84def1e
zhangke <kezhang404@gmail.com>
2024-07-07 22:04:07 +0800

    optimize import process

--- framework/src/main/java/com/zhangke/framework/loadable/lazycolumn/LoadableInlineVideoLazyColumn.kt
+++ framework/src/main/java/com/zhangke/framework/loadable/lazycolumn/LoadableInlineVideoLazyColumn.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -20,16 +19,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.zhangke.framework.composable.ConsumeFlow
 import com.zhangke.framework.composable.inline.InlineVideoLazyColumn
-import com.zhangke.framework.loadable.previous.LoadPreviousPageItem
-import com.zhangke.framework.loadable.previous.LoadPreviousPageUiState
-import com.zhangke.framework.loadable.previous.PreviousPageLoadingState
 import com.zhangke.framework.utils.LoadState
 
 @OptIn(ExperimentalMaterialApi::class)

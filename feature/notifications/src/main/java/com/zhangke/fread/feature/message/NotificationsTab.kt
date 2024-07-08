854cf245210436ceefeaa9ea12ef158d52da2231
zhangke <kezhang404@gmail.com>
2024-07-08 22:38:58 +0800

    optimize main bottom navigation bar ui

--- feature/notifications/src/main/java/com/zhangke/fread/feature/message/NotificationsTab.kt
+++ feature/notifications/src/main/java/com/zhangke/fread/feature/message/NotificationsTab.kt
@@ -5,16 +5,18 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import com.zhangke.fread.feature.message.screens.home.NotificationsHomeScreen
+import com.zhangke.fread.feature.notifications.R
 
 class NotificationsTab(private val tabIndex: UShort) : Tab {
 
     override val options: TabOptions
         @Composable get() {
-            val icon = rememberVectorPainter(Icons.Default.Notifications)
+            val icon = painterResource(id = R.drawable.ic_notification_tab)
             return remember {
                 TabOptions(
                     index = tabIndex, title = "Notifications", icon = icon

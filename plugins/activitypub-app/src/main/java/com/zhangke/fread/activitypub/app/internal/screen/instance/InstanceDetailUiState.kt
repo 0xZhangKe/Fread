15907905c4f9c1988ccf78ba3a8db3ca020e8566
zhangke <kezhang404@gmail.com>
2024-07-15 20:15:33 +0800

    improve instance detail page ui and ux

--- plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/screen/instance/InstanceDetailUiState.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/screen/instance/InstanceDetailUiState.kt
@@ -2,9 +2,11 @@ package com.zhangke.fread.activitypub.app.internal.screen.instance
 
 import com.zhangke.activitypub.entities.ActivityPubInstanceEntity
 import com.zhangke.framework.network.FormalBaseUrl
+import com.zhangke.fread.status.author.BlogAuthor
 
 data class InstanceDetailUiState(
     val loading: Boolean,
     val baseUrl: FormalBaseUrl?,
     val instance: ActivityPubInstanceEntity?,
+    val modAccount: BlogAuthor?,
 )

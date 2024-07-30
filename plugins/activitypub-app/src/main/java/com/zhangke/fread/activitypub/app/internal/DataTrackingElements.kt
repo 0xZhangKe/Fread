e5ecb1d28813e39755c3169842cdf8719690a6b8
zhangke <kezhang404@gmail.com>
2024-07-30 23:17:56 +0800

    optimize ui and ux

--- plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/DataTrackingElements.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/DataTrackingElements.kt
@@ -1,11 +1,16 @@
 package com.zhangke.fread.activitypub.app.internal
 
-object DataTrackingElements {
-
-}
-
 object ApNotificationElements {
 
     const val ALL = "apNotificationAll"
     const val MENTION = "apNotificationMention"
+}
+
+object ActivityPubDataElements {
+
+    const val INSTANCE_DETAIL_OPEN_IN_BROWSER = "instanceDetailOpenInBrowser"
+    const val INSTANCE_DETAIL_COPY_LINK = "instanceDetailCopyLink"
+
+    const val USER_DETAIL_OPEN_IN_BROWSER = "userDetailOpenInBrowser"
+    const val USER_DETAIL_COPY_LINK = "userDetailCopyLink"
 }

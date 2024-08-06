3f0ac0f07ae0f5ee6ccb654674cf2fc3d590b97d
ZhangKe <kezhang404@gmail.com>
2024-08-06 17:58:21 +0800

    support mute operation and view muted users

---
+++ plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/screen/user/block/BlockedUserListScreen.kt
@@ -0,0 +1,12 @@
+package com.zhangke.fread.activitypub.app.internal.screen.user.block
+
+import androidx.compose.runtime.Composable
+import com.zhangke.fread.common.page.BaseScreen
+
+class BlockedUserListScreen: BaseScreen() {
+
+    @Composable
+    override fun Content() {
+        super.Content()
+    }
+}

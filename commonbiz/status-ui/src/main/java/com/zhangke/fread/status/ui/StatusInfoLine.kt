5c63550fce54438f4f8475d4ce9af8e38aff6beb
ZhangKe <kezhang404@gmail.com>
2024-08-09 17:30:36 +0800

    refactor status ui

--- commonbiz/status-ui/src/main/java/com/zhangke/fread/status/ui/StatusInfoLine.kt
+++ commonbiz/status-ui/src/main/java/com/zhangke/fread/status/ui/StatusInfoLine.kt
@@ -1,43 +1,21 @@
 package com.zhangke.fread.status.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.LockOpen
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import coil.imageLoader
-import com.zhangke.framework.composable.freadPlaceholder
 import com.zhangke.fread.analytics.reportClick
 import com.zhangke.fread.common.status.model.StatusUiInteraction
 import com.zhangke.fread.status.author.BlogAuthor
@@ -45,7 +23,6 @@ import com.zhangke.fread.status.model.StatusVisibility
 import com.zhangke.fread.status.ui.action.StatusMoreInteractionIcon
 import com.zhangke.fread.status.ui.richtext.FreadRichText
 import com.zhangke.fread.status.ui.style.StatusStyle
-import com.zhangke.fread.status.ui.threads.StatusThread
 
 /**
  * Status 头部信息行，主要包括头像，
@@ -86,7 +63,7 @@ fun StatusInfoLine(
         Column(
             modifier = Modifier
                 .weight(1F)
-                .padding(start = 8.dp, end = 6.dp),
+                .padding(start = style.infoLineStyle.nameToAvatarSpacing, end = 6.dp),
         ) {
             FreadRichText(
                 modifier = Modifier

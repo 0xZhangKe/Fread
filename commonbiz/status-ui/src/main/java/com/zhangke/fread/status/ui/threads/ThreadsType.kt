806b613a2e75c7c1045fb2f5d9be35165ec07e89
zhangke <kezhang404@gmail.com>
2024-08-09 21:35:17 +0800

    refactor staus ui

--- commonbiz/status-ui/src/main/java/com/zhangke/fread/status/ui/threads/ThreadsType.kt
+++ commonbiz/status-ui/src/main/java/com/zhangke/fread/status/ui/threads/ThreadsType.kt
@@ -3,7 +3,24 @@ package com.zhangke.fread.status.ui.threads
 enum class ThreadsType {
 
     NONE,
+
+    /**
+     * 第一个评论
+     */
     FIRST_ANCESTOR,
+
+    /**
+     * 帖子上级评论
+     */
     ANCESTOR,
+
+    /**
+     * 锚点帖子，且没有父级
+     */
+    ANCHOR_FIRST,
+
+    /**
+     * 锚点帖子
+     */
     ANCHOR,
 }

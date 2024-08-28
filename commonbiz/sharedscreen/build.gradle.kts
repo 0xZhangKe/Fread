f75db36878938090bbbb91845efe79c9cac6e0dc
seiko <seiko_des@outlook.com>
2024-08-28 11:53:57 +0800

    migrate jodatime to kotlin.datetime

--- commonbiz/sharedscreen/build.gradle.kts
+++ commonbiz/sharedscreen/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.converter.gson)
     implementation(libs.gson)
-    implementation(libs.joda.time)
     implementation(libs.auto.service.annotations)
     ksp(libs.auto.service.ksp)
     implementation(libs.androidx.paging.runtime)
--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.converter.gson)
     implementation(libs.gson)
-    implementation(libs.joda.time)
     implementation(libs.auto.service.annotations)
     ksp(libs.auto.service.ksp)
     implementation(libs.bundles.voyager)

defd2f77e30b6c6f10d5975c023160665b35ba1a
seiko <seiko_des@outlook.com>
2024-08-28 19:10:50 +0800

    migrate retrofit2 to ktor

--- plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/db/converter/EmojiListConverter.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/db/converter/EmojiListConverter.kt
@@ -1,19 +1,19 @@
 package com.zhangke.fread.activitypub.app.internal.db.converter
 
 import androidx.room.TypeConverter
-import com.google.gson.reflect.TypeToken
-import com.zhangke.framework.architect.json.globalGson
+import com.zhangke.framework.architect.json.globalJson
 import com.zhangke.fread.status.model.Emoji
+import kotlinx.serialization.encodeToString
 
 class EmojiListConverter {
 
     @TypeConverter
     fun toJsonString(blogAuthor: List<Emoji>): String {
-        return globalGson.toJson(blogAuthor)
+        return globalJson.encodeToString(blogAuthor)
     }
 
     @TypeConverter
     fun toEmoji(jsonString: String): List<Emoji> {
-        return globalGson.fromJson(jsonString, object : TypeToken<List<Emoji>>() {}.type)
+        return globalJson.decodeFromString(jsonString)
     }
 }
--- plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/db/converter/RelationshipSeveranceEventConverter.kt
+++ plugins/activitypub-app/src/main/java/com/zhangke/fread/activitypub/app/internal/db/converter/RelationshipSeveranceEventConverter.kt
@@ -1,19 +1,20 @@
 package com.zhangke.fread.activitypub.app.internal.db.converter
 
 import androidx.room.TypeConverter
-import com.zhangke.framework.architect.json.globalGson
+import com.zhangke.framework.architect.json.globalJson
 import com.zhangke.fread.activitypub.app.internal.model.RelationshipSeveranceEvent
+import kotlinx.serialization.encodeToString
 
 class RelationshipSeveranceEventConverter {
 
     @TypeConverter
     fun fromType(platform: RelationshipSeveranceEvent?): String? {
-        return globalGson.toJson(platform)
+        return platform?.let { globalJson.encodeToString(it) }
     }
 
     @TypeConverter
     fun toType(text: String?): RelationshipSeveranceEvent? {
         if (text.isNullOrEmpty()) return null
-        return globalGson.fromJson(text, RelationshipSeveranceEvent::class.java)
+        return globalJson.decodeFromString(text)
     }
 }

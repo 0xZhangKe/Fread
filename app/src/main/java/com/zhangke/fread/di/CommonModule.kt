501821dbcbd9720cbc6f54c8fadbf1de0763159c
seiko <seiko_des@outlook.com>
2024-08-30 22:01:00 +0800

    remove di for bizframework

--- app/src/main/java/com/zhangke/fread/di/CommonModule.kt
+++ app/src/main/java/com/zhangke/fread/di/CommonModule.kt
@@ -2,10 +2,13 @@ package com.zhangke.fread.di
 
 import android.content.Context
 import com.zhangke.framework.utils.appContext
+import com.zhangke.fread.status.IStatusProvider
+import com.zhangke.fread.status.StatusProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,4 +19,13 @@ class CommonModule {
     fun provideApplicationContext(): Context {
         return appContext
     }
+
+    @Provides
+    @Singleton
+    fun provideProvideStatusProvider(
+        providers: Set<@JvmSuppressWildcards IStatusProvider>,
+    ): StatusProvider {
+        return StatusProvider(providers)
+    }
+
 }

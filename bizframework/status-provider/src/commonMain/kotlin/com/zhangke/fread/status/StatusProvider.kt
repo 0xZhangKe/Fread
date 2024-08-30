1b549bf92737ec590ba530e839e046b4a60f0b72
seiko <seiko_des@outlook.com>
2024-08-30 22:01:00 +0800

    remove di for bizframework

--- bizframework/status-provider/src/commonMain/kotlin/com/zhangke/fread/status/StatusProvider.kt
+++ bizframework/status-provider/src/commonMain/kotlin/com/zhangke/fread/status/StatusProvider.kt
@@ -1,18 +1,23 @@
 package com.zhangke.fread.status
 
 import com.zhangke.fread.status.account.AccountManager
+import com.zhangke.fread.status.account.IAccountManager
+import com.zhangke.fread.status.platform.IPlatformResolver
 import com.zhangke.fread.status.platform.PlatformResolver
+import com.zhangke.fread.status.screen.IStatusScreenProvider
 import com.zhangke.fread.status.screen.StatusScreenProvider
+import com.zhangke.fread.status.search.ISearchEngine
 import com.zhangke.fread.status.search.SearchEngine
+import com.zhangke.fread.status.source.IStatusSourceResolver
 import com.zhangke.fread.status.source.StatusSourceResolver
+import com.zhangke.fread.status.status.IStatusResolver
 import com.zhangke.fread.status.status.StatusResolver
-import javax.inject.Inject
 
 /**
  * Created by ZhangKe on 2022/12/9.
  */
-class StatusProvider @Inject constructor(
-    providers: Set<@JvmSuppressWildcards IStatusProvider>,
+class StatusProvider constructor(
+    providers: Set<IStatusProvider>,
 ) {
 
     val screenProvider = StatusScreenProvider(providers.map { it.screenProvider })
@@ -28,3 +33,17 @@ class StatusProvider @Inject constructor(
     val accountManager = AccountManager(providers.map { it.accountManager })
 }
 
+interface IStatusProvider {
+
+    val screenProvider: IStatusScreenProvider
+
+    val platformResolver: IPlatformResolver
+
+    val searchEngine: ISearchEngine
+
+    val statusResolver: IStatusResolver
+
+    val statusSourceResolver: IStatusSourceResolver
+
+    val accountManager: IAccountManager
+}

f5936d6e097a743d8b6f33c4eb94db4edf80411a
zhangke <kezhang404@gmail.com>
2024-08-30 20:38:05 +0800

    add mention text highlight

--- commonbiz/src/main/java/com/zhangke/fread/common/utils/MentionTextUtil.kt
+++ commonbiz/src/main/java/com/zhangke/fread/common/utils/MentionTextUtil.kt
@@ -29,32 +29,23 @@ object MentionTextUtil {
         }
     }
 
-//    fun findMentionList(text: String): List<TextRange> {
-//        if (text.isEmpty()) return emptyList()
-//        val list = mutableListOf<TextRange>()
-//        val matcher = TypingMentionMather()
-//        var index = text.length - 1
-//        val chars = text.toCharArray()
-//        while (index > 0) {
-//            val range = matcher.findTypingMentionRange(chars, index)
-//            if (range)
-//        }
-//
-//        return list
-//    }
+    fun findMentionList(text: String): List<TextRange> {
+        if (text.isEmpty()) return emptyList()
+        return TextMentionMatcher().findMentionRanges(text.toCharArray())
+    }
 
     private fun findTypingMentionRange(text: TextFieldValue): TextRange? {
         if (text.selection.length != 0) return null
         if (text.selection.start <= 0) return null
         val chars = text.text.toCharArray()
-        return TypingMentionMather().findTypingMentionRange(
+        return TypingMentionMatcher().findTypingMentionRange(
             chars = chars,
             startIndex = text.selection.start - 1,
         )
     }
 }
 
-class TypingMentionMather {
+class TypingMentionMatcher {
 
     companion object {
 
@@ -111,47 +102,65 @@ class TypingMentionMather {
         }
         return null
     }
+}
 
-//    fun findMentionRanges(chars: CharArray): List<TextRange> {
-//        if (chars.isEmpty()) return emptyList()
-//        var state = STATE_INIT
-//        var index = startIndex
-//        var firstMentionFlagIndex = -1
-//        while (index >= 0) {
-//            when (state) {
-//                STATE_INIT -> {
-//                    state = STATE_SCANNING_NAME_OR_HOST
-//                }
-//
-//                STATE_SCANNING_NAME_OR_HOST -> {
-//                    if (chars[index].validateWebFinerSymbol) {
-//                        index--
-//                    } else if (chars[index].isMentionFlag) {
-//                        firstMentionFlagIndex = index
-//                        index--
-//                        state = STATE_SCANNING_NAME
-//                    } else {
-//                        return null
-//                    }
-//                }
-//
-//                STATE_SCANNING_NAME -> {
-//                    if (chars[index].validateWebFinerSymbol) {
-//                        index--
-//                    } else if (chars[index].isMentionFlag) {
-//                        if (firstMentionFlagIndex - index == 1) break
-//                        return TextRange(index, startIndex + 1)
-//                    } else {
-//                        break
-//                    }
-//                }
-//            }
-//        }
-//        if (state == STATE_SCANNING_NAME) {
-//            return TextRange(firstMentionFlagIndex, startIndex + 1)
-//        }
-//        return null
-//    }
+class TextMentionMatcher {
+
+    companion object {
+
+        private const val STATE_SCANNING_FIRST_MENTION_FLAG = 0
+        private const val STATE_SCANNING_NAME = STATE_SCANNING_FIRST_MENTION_FLAG + 1
+        private const val STATE_SCANNING_HOST = STATE_SCANNING_NAME + 1
+    }
+
+    fun findMentionRanges(chars: CharArray): List<TextRange> {
+        if (chars.isEmpty()) return emptyList()
+        val list = mutableListOf<TextRange>()
+        var state = STATE_SCANNING_FIRST_MENTION_FLAG
+        var index = 0
+        var nameMentionFlagIndex = -1
+        while (index < chars.size) {
+            val char = chars[index]
+            when (state) {
+                STATE_SCANNING_FIRST_MENTION_FLAG -> {
+                    if (char.isMentionFlag) {
+                        nameMentionFlagIndex = index
+                        state = STATE_SCANNING_NAME
+                    }
+                    index++
+                }
+
+                STATE_SCANNING_NAME -> {
+                    if (char.isMentionFlag) {
+                        state = STATE_SCANNING_HOST
+                    } else if (!char.validateWebFinerSymbol) {
+                        list += TextRange(nameMentionFlagIndex, index)
+                        nameMentionFlagIndex = -1
+                        state = STATE_SCANNING_FIRST_MENTION_FLAG
+                    }
+                    index++
+                }
+
+                STATE_SCANNING_HOST -> {
+                    if (!char.validateWebFinerSymbol) {
+                        if (index - nameMentionFlagIndex > 1) {
+                            list += TextRange(nameMentionFlagIndex, index)
+                        }
+                        state = STATE_SCANNING_FIRST_MENTION_FLAG
+                    }
+                    index++
+                }
+            }
+        }
+
+        if (state == STATE_SCANNING_HOST && index - nameMentionFlagIndex > 1) {
+            list += TextRange(nameMentionFlagIndex, index)
+        }
+        if (state == STATE_SCANNING_NAME){
+            list += TextRange(nameMentionFlagIndex, index)
+        }
+        return list
+    }
 }
 
 private val Char.isMentionFlag: Boolean get() = this == '@'

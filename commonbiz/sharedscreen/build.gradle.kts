2c8996576eef0c9d8ea3c13dc4c74b354fe28e1f
seiko <seiko_des@outlook.com>
2024-09-03 12:51:30 +0800

    refactor commonbiz/sharedscreen to kmp

--- commonbiz/sharedscreen/build.gradle.kts
+++ commonbiz/sharedscreen/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fread.project.framework")
+    id("fread.project.framework.kmp")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
@@ -9,48 +9,64 @@ android {
     namespace = "com.zhangke.fread.commonbiz.shared.screen"
 }
 
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(path = ":framework"))
+                implementation(project(path = ":commonbiz"))
+                implementation(project(path = ":bizframework:status-provider"))
+                implementation(project(path = ":commonbiz:status-ui"))
+                implementation(project(":commonbiz:analytics"))
+
+                implementation(libs.kotlinx.serialization.core)
+                implementation(libs.kotlinx.serialization.json)
+
+                implementation(libs.bundles.voyager)
+
+                implementation(libs.imageLoader)
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        androidMain {
+            dependencies {
+                implementation(libs.androidx.core.ktx)
+                implementation(libs.androidx.appcompat)
+                implementation(libs.androidx.annotation)
+                implementation(libs.androidx.compose.constraintlayout)
+                implementation(libs.bundles.androidx.fragment)
+                implementation(libs.bundles.androidx.activity)
+                implementation(libs.bundles.androidx.preference)
+                implementation(libs.bundles.androidx.datastore)
+                implementation(libs.bundles.androidx.collection)
+                implementation(libs.androidx.browser)
+
+                implementation(libs.androidx.room)
+                implementation(libs.androidx.room.ktx)
+
+                implementation(libs.androidx.paging.runtime)
+                implementation(libs.androidx.paging.compose)
+
+                implementation(libs.okhttp3)
+                implementation(libs.okhttp3.logging)
+
+                implementation(libs.auto.service.annotations)
+                implementation(libs.filt.annotaions)
+                implementation(libs.hilt)
+                implementation(libs.krouter.core)
+            }
+        }
+    }
+}
+
 dependencies {
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-
-    implementation(project(path = ":framework"))
-    implementation(project(path = ":commonbiz"))
-    implementation(project(path = ":bizframework:status-provider"))
-    implementation(project(path = ":commonbiz:status-ui"))
-    implementation(project(":commonbiz:analytics"))
-
-    implementation(libs.bundles.kotlin)
-    implementation(libs.androidx.core)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.annotation)
-    implementation(libs.androidx.compose.constraintlayout)
-    implementation(libs.bundles.androidx.fragment)
-    implementation(libs.bundles.androidx.activity)
-    implementation(libs.bundles.androidx.preference)
-    implementation(libs.bundles.androidx.datastore)
-    implementation(libs.bundles.androidx.collection)
-    implementation(libs.androidx.browser)
-    implementation(libs.androidx.room)
-    implementation(libs.androidx.room.ktx)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.filt.annotaions)
-    ksp(libs.filt.compiler)
-    implementation(libs.hilt)
-    ksp(libs.hilt.compiler)
-    implementation(libs.imageLoader)
-    implementation(libs.okhttp3)
-    implementation(libs.okhttp3.logging)
-    implementation(libs.auto.service.annotations)
-    ksp(libs.auto.service.ksp)
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.compose)
-    implementation(libs.bundles.voyager)
-    implementation(libs.krouter.core)
-    ksp(libs.krouter.compiler)
-
-    implementation(libs.kotlinx.serialization.core)
-    implementation(libs.kotlinx.serialization.json)
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspAndroid", libs.filt.compiler)
+    add("kspAndroid", libs.hilt.compiler)
+    add("kspAndroid", libs.auto.service.ksp)
+    add("kspAndroid", libs.krouter.compiler)
 }

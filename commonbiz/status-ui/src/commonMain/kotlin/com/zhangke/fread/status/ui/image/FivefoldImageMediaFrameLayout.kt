7d46ad7df5541d70a693ede04178651660524ae3
seiko <seiko_des@outlook.com>
2024-09-03 11:19:43 +0800

    refactor commonbiz/status-ui to kmp

--- commonbiz/status-ui/src/commonMain/kotlin/com/zhangke/fread/status/ui/image/FivefoldImageMediaFrameLayout.kt
+++ commonbiz/status-ui/src/commonMain/kotlin/com/zhangke/fread/status/ui/image/FivefoldImageMediaFrameLayout.kt
@@ -1,6 +1,5 @@
 package com.zhangke.fread.status.ui.image
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -9,7 +8,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp

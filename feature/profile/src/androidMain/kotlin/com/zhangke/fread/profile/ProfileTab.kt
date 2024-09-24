4e844c25c85b72b99ebb49316403cc5a7a6c53c6
seiko <seiko_des@outlook.com>
2024-09-24 13:14:53 +0800

    migrate profile res to kmp

--- feature/profile/src/androidMain/kotlin/com/zhangke/fread/profile/ProfileTab.kt
+++ feature/profile/src/androidMain/kotlin/com/zhangke/fread/profile/ProfileTab.kt
@@ -2,17 +2,19 @@ package com.zhangke.fread.profile
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.res.painterResource
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.zhangke.fread.feature.profile.Res
+import com.zhangke.fread.feature.profile.ic_profile_tab
 import com.zhangke.fread.profile.screen.home.ProfileHomePage
+import org.jetbrains.compose.resources.painterResource
 
 class ProfileTab(private val tabIndex: UShort) : Tab {
 
     override val options: TabOptions
         @Composable get() {
-            val icon = painterResource(id = R.drawable.ic_profile_tab)
+            val icon = painterResource(Res.drawable.ic_profile_tab)
             return remember {
                 TabOptions(
                     index = tabIndex, title = "Profile", icon = icon

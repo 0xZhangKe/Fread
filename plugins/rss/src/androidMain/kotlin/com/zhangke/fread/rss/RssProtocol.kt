a5f62ae19614dea190885688f610e5b863e0c2c2
seiko <seiko_des@outlook.com>
2024-09-24 14:31:05 +0800

    migrate rss res to kmp

--- plugins/rss/src/androidMain/kotlin/com/zhangke/fread/rss/RssProtocol.kt
+++ plugins/rss/src/androidMain/kotlin/com/zhangke/fread/rss/RssProtocol.kt
@@ -1,12 +1,12 @@
 package com.zhangke.fread.rss
 
-import android.content.Context
 import com.zhangke.fread.status.model.RSS_PROTOCOL_ID
 import com.zhangke.fread.status.model.StatusProviderProtocol
+import org.jetbrains.compose.resources.getString
 
-fun createRssProtocol(context: Context): StatusProviderProtocol {
+suspend fun createRssProtocol(): StatusProviderProtocol {
     return StatusProviderProtocol(
         id = RSS_PROTOCOL_ID,
-        name = context.getString(R.string.rss_protocol_name),
+        name = getString(Res.string.rss_protocol_name),
     )
 }

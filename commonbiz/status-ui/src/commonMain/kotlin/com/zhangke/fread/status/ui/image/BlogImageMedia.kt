eb3765a166396a74844db64c1879613e04225a68
ZhangKe <kezhang404@gmail.com>
2024-09-25 17:21:39 +0800

    add image alt text to timeline

--- commonbiz/status-ui/src/commonMain/kotlin/com/zhangke/fread/status/ui/image/BlogImageMedia.kt
+++ commonbiz/status-ui/src/commonMain/kotlin/com/zhangke/fread/status/ui/image/BlogImageMedia.kt
@@ -4,16 +4,27 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -153,6 +164,7 @@ internal fun BlogImageLayout(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun BlogImage(
     modifier: Modifier,
@@ -170,22 +182,22 @@ internal fun BlogImage(
             )
         }
     }
-    val mediaModifier = modifier.blurhash(media.blurhash)
-    if (media.type == BlogMediaType.GIFV) {
-        Box(modifier = mediaModifier) {
+
+    Box(modifier = modifier.blurhash(media.blurhash)) {
+        if (!hideContent) {
             AutoSizeImage(
-                request = remember(hideContent) {
+                request = remember {
                     ImageRequest {
-                        if (!hideContent) {
-                            data(media.previewUrl)
-                        }
+                        data(media.previewUrl)
                     }
                 },
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop,
                 contentDescription = media.description.ifNullOrEmpty { "Blog Image Media" },
             )
+        }
 
+        if (media.type == BlogMediaType.GIFV) {
             Icon(
                 modifier = Modifier
                     .size(32.dp)
@@ -195,19 +207,40 @@ internal fun BlogImage(
                 contentDescription = "Play",
             )
         }
-    } else {
-        Box(modifier = mediaModifier) {
-            if (!hideContent) {
-                AutoSizeImage(
-                    request = remember {
-                        ImageRequest {
-                            data(media.previewUrl)
-                        }
-                    },
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = media.description.ifNullOrEmpty { "Blog Image Media" },
-                )
+        if (!media.description.isNullOrEmpty()) {
+            var showBottomSheet by remember { mutableStateOf(false) }
+            Surface(
+                modifier = Modifier.align(Alignment.BottomStart)
+                    .padding(start = 2.dp, bottom = 2.dp),
+                onClick = {
+                    showBottomSheet = true
+                },
+                shape = RoundedCornerShape(4.dp),
+                enabled = true,
+                color = Color.Black.copy(alpha = 0.6F),
+                contentColor = Color.White,
+                content = {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                        text = "ALT",
+                        style = MaterialTheme.typography.labelSmall,
+                    )
+                },
+            )
+
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    onDismissRequest = { showBottomSheet = false },
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 28.dp)
+                            .wrapContentHeight()
+                    ) {
+                        Text(text = media.description.orEmpty())
+                    }
+                }
             }
         }
     }

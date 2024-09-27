8b545a0a8974c02bbafd8ab0df6e772e8007a478
seiko <seiko_des@outlook.com>
2024-09-27 15:40:25 +0800

    fix some "Not yet implemented" in ios target

--- commonbiz/analytics/src/iosMain/kotlin/com/zhangke/fread/analytics/TrackingScreenEvent.ios.kt
+++ commonbiz/analytics/src/iosMain/kotlin/com/zhangke/fread/analytics/TrackingScreenEvent.ios.kt
@@ -4,17 +4,17 @@ import cafe.adriel.voyager.core.screen.Screen
 import com.zhangke.framework.composable.PagerTab
 
 internal actual val Screen.pageEventName: String
-    get() = TODO("Not yet implemented")
+    get() = this::class.simpleName ?: error("unknown pageEventName $this")
 
 internal actual val PagerTab.pageEventName: String
-    get() = TODO("Not yet implemented")
+    get() = this::class.simpleName ?: error("unknown pageEventName $this")
 
 internal actual fun Screen?.generateEventId(): String {
     if (this == null) return "unknownPage"
-    TODO("Not yet implemented")
+    return "${this::class.simpleName}@${this.hashCode()}"
 }
 
 internal actual fun PagerTab?.generateEventId(): String {
     if (this == null) return "unknownPage"
-    TODO("Not yet implemented")
+    return "${this::class.simpleName}@${this.hashCode()}"
 }

5f4938642387a71d9054a083dbbcc2c7ebfc76d5
zhangke <kezhang404@gmail.com>
2024-10-06 18:09:49 +0800

    support download status video

--- framework/src/androidMain/kotlin/com/zhangke/framework/media/MediaFileHelper.kt
+++ framework/src/androidMain/kotlin/com/zhangke/framework/media/MediaFileHelper.kt
@@ -1,18 +1,31 @@
 package com.zhangke.framework.media
 
 import android.content.Context
+import com.zhangke.framework.architect.coroutines.ApplicationScope
 import com.zhangke.framework.toast.showFileSaveFailedToast
 import com.zhangke.framework.toast.showFileSaveSuccessToast
 import com.zhangke.framework.toast.showFileSavingToast
+import kotlinx.coroutines.launch
 
 object MediaFileHelper {
 
-    suspend fun saveImageToGallery(context: Context, url: String){
+    suspend fun saveImageToGallery(context: Context, url: String) {
         showFileSavingToast()
         if (MediaFileUtil.saveImageToGallery(context, url)) {
             showFileSaveSuccessToast()
         } else {
             showFileSaveFailedToast()
+        }
+    }
+
+    suspend fun saveVideoToGallery(context: Context, url: String) {
+        showFileSavingToast()
+        ApplicationScope.launch {
+            if (MediaFileUtil.saveVideoToGallery(context, url)) {
+                showFileSaveSuccessToast()
+            } else {
+                showFileSaveFailedToast()
+            }
         }
     }
 }

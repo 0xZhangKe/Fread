48f5ada0b62eb3182400c55907ea9d5c0ab98f17
zhangke <kezhang404@gmail.com>
2024-10-15 23:08:00 +0800

    add crypto

--- feature/notifications/src/commonMain/kotlin/com/zhangke/fread/feature/message/push/PushManager.kt
+++ feature/notifications/src/commonMain/kotlin/com/zhangke/fread/feature/message/push/PushManager.kt
@@ -2,6 +2,8 @@ package com.zhangke.fread.feature.message.push
 
 import com.zhangke.fread.common.config.FreadConfigManager
 import me.tatarka.inject.annotations.Inject
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class PushManager @Inject constructor(
     private val freadConfigManager: FreadConfigManager,
@@ -12,8 +14,9 @@ class PushManager @Inject constructor(
         private const val ENDPOINT_URL = "https://localhost/push/relay/send"
     }
 
+    @OptIn(ExperimentalEncodingApi::class)
     suspend fun getEndpointUrl(accountId: String): String {
         val deviceId = freadConfigManager.getDeviceId()
-        return "$ENDPOINT_URL/$deviceId/$accountId"
+        return "$ENDPOINT_URL/$deviceId/${Base64.UrlSafe.encode(accountId.encodeToByteArray())}"
     }
 }

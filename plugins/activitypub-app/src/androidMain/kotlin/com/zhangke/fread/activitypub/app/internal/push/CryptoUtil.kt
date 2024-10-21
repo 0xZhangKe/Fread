0b12a8ff17124c6f2188ad147db6743bedc58921
ZhangKe <kezhang404@gmail.com>
2024-10-21 17:48:52 +0800

    add PushInfoRepo

--- plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/CryptoUtil.kt
+++ plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/CryptoUtil.kt
@@ -2,11 +2,8 @@ package com.zhangke.fread.activitypub.app.internal.push
 
 import android.util.Base64
 import java.security.KeyPairGenerator
-import java.security.PublicKey
 import java.security.SecureRandom
-import java.security.interfaces.ECPublicKey
 import java.security.spec.ECGenParameterSpec
-import java.util.Arrays
 
 class CryptoUtil {
 
@@ -23,31 +20,19 @@ class CryptoUtil {
         val authKey = ByteArray(16)
         val secureRandom = SecureRandom()
         secureRandom.nextBytes(authKey)
-        val encodedPublicKey =
-            Base64.encodeToString(serializeRawPublicKey(keyPair.public), base64Flag)
+        val encodedPrivateKey = Base64.encodeToString(keyPair.private.encoded, base64Flag)
+        val encodedPublicKey = Base64.encodeToString(keyPair.public.encoded, base64Flag)
         val pushAuthKey = Base64.encodeToString(authKey, base64Flag)
         return CryptoKeys(
+            privateKey = encodedPrivateKey,
             publicKey = encodedPublicKey,
             authKey = pushAuthKey,
         )
     }
-
-    private fun serializeRawPublicKey(key: PublicKey): ByteArray {
-        val point = (key as ECPublicKey).w
-        var x = point.affineX.toByteArray()
-        var y = point.affineY.toByteArray()
-        if (x.size > 32) x = Arrays.copyOfRange(x, x.size - 32, x.size)
-        if (y.size > 32) y = Arrays.copyOfRange(y, y.size - 32, y.size)
-        val result = ByteArray(65)
-        result[0] = 4
-        System.arraycopy(x, 0, result, 1 + (32 - x.size), x.size)
-        System.arraycopy(y, 0, result, result.size - y.size, y.size)
-        return result
-    }
 }
 
 data class CryptoKeys(
+    val privateKey: String,
     val publicKey: String,
     val authKey: String,
 )
-
---
+++ plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/PushInfoRepo.kt
@@ -0,0 +1,69 @@
+package com.zhangke.fread.activitypub.app.internal.push
+
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import me.tatarka.inject.annotations.Inject
+
+private const val DB_VERSION = 1
+private const val TABLE_NAME = "PushInfo"
+
+@Entity(tableName = TABLE_NAME)
+data class PushInfo(
+    @PrimaryKey val accountId: String,
+    val publicKey: String,
+    val privateKey: String,
+    val authKey: String,
+)
+
+@Dao
+interface PushInfoDao {
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE accountId = :accountId")
+    suspend fun getPushInfo(accountId: String): PushInfo?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(info: PushInfo)
+
+    @Query("DELETE FROM $TABLE_NAME WHERE accountId = :accountId")
+    suspend fun delete(accountId: String)
+}
+
+@Database(
+    entities = [PushInfo::class],
+    version = DB_VERSION,
+    exportSchema = false,
+)
+abstract class PushInfoDatabase : RoomDatabase() {
+
+    abstract fun pushInfoDao(): PushInfoDao
+
+    companion object {
+
+        const val DB_NAME = "ActivityPubPushInfo.db"
+    }
+}
+
+class PushInfoRepo @Inject constructor(
+    private val database: PushInfoDatabase,
+) {
+
+    private val pushDao = database.pushInfoDao()
+
+    suspend fun getPushInfo(accountId: String): PushInfo? {
+        return pushDao.getPushInfo(accountId)
+    }
+
+    suspend fun insert(info: PushInfo) {
+        pushDao.insert(info)
+    }
+
+    suspend fun delete(accountId: String) {
+        pushDao.delete(accountId)
+    }
+}

f04e858366fcf7add5828e2fa9f90dd01c3950e5
zhangke <kezhang404@gmail.com>
2024-10-22 22:00:03 +0800

    complete crypto

--- plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/CryptoUtil.kt
+++ plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/CryptoUtil.kt
@@ -1,11 +1,13 @@
 package com.zhangke.fread.activitypub.app.internal.push
 
 import android.util.Base64
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 import java.security.KeyFactory
 import java.security.KeyPairGenerator
 import java.security.PublicKey
 import java.security.SecureRandom
-import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.ECPublicKey
 import java.security.spec.ECGenParameterSpec
 import java.security.spec.PKCS8EncodedKeySpec
@@ -13,6 +15,7 @@ import java.security.spec.X509EncodedKeySpec
 import java.util.Arrays
 import javax.crypto.Cipher
 import javax.crypto.KeyAgreement
+import javax.crypto.Mac
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
@@ -20,24 +23,55 @@ object CryptoUtil {
 
     private const val EC_CURVE_NAME = "prime256v1"
 
+    private val P256_HEAD = byteArrayOf(
+        0x30.toByte(),
+        0x59.toByte(),
+        0x30.toByte(),
+        0x13.toByte(),
+        0x06.toByte(),
+        0x07.toByte(),
+        0x2a.toByte(),
+        0x86.toByte(),
+        0x48.toByte(),
+        0xce.toByte(),
+        0x3d.toByte(),
+        0x02.toByte(),
+        0x01.toByte(),
+        0x06.toByte(),
+        0x08.toByte(),
+        0x2a.toByte(),
+        0x86.toByte(),
+        0x48.toByte(),
+        0xce.toByte(),
+        0x3d.toByte(),
+        0x03.toByte(),
+        0x01.toByte(),
+        0x07.toByte(),
+        0x03.toByte(),
+        0x42.toByte(),
+        0x00.toByte()
+    )
+
     private val reBase64UrlSafe = """[_-]""".toRegex()
 
     fun generate(): CryptoKeys {
         val base64Flag = Base64.URL_SAFE or Base64.NO_WRAP or Base64.NO_PADDING
         val generator = KeyPairGenerator.getInstance("EC")
-        val spec = ECGenParameterSpec(EC_CURVE_NAME)
-        generator.initialize(spec)
+        generator.initialize(ECGenParameterSpec(EC_CURVE_NAME))
         val keyPair = generator.generateKeyPair()
+        val privateKey = Base64.encodeToString(keyPair.private.encoded, base64Flag)
+        val publicKey = Base64.encodeToString(keyPair.public.encoded, base64Flag)
         val authKey = ByteArray(16)
         val secureRandom = SecureRandom()
         secureRandom.nextBytes(authKey)
-        val encodedPrivateKey = Base64.encodeToString(keyPair.private.encoded, base64Flag)
-        val encodedPublicKey =
-            Base64.encodeToString(serializeRawPublicKey(keyPair.public), base64Flag)
         val pushAuthKey = Base64.encodeToString(authKey, base64Flag)
         return CryptoKeys(
-            privateKey = encodedPrivateKey,
-            publicKey = encodedPublicKey,
+            privateKey = privateKey,
+            publicKey = publicKey,
+            encodedPublicKey = Base64.encodeToString(
+                serializeRawPublicKey(keyPair.public),
+                base64Flag,
+            ),
             authKey = pushAuthKey,
         )
     }
@@ -56,42 +90,104 @@ object CryptoUtil {
     }
 
     fun decryptData(
-        privateKey: String,
+        keys: PushInfo,
         serverPublicKeyEncoded: String,
+        encryptionSalt: String,
+        contentEncoding: String,
         data: ByteArray,
     ): String {
         val base64Flag = Base64.URL_SAFE or Base64.NO_WRAP or Base64.NO_PADDING
-        val keySpec = PKCS8EncodedKeySpec(Base64.decode(privateKey, base64Flag))
-        val ecPrivateKey = KeyFactory.getInstance("EC").generatePrivate(keySpec) as ECPrivateKey
+        val salt = encryptionSalt.decodeBase64()
+        val serverKey = deserializeRawPublicKey(serverPublicKeyEncoded.decodeBase64())!!
+
         val keyFactory = KeyFactory.getInstance("EC")
-        val publicKeyBytes = serverPublicKeyEncoded.decodeBase64()
-        val serverPublicKey =
-            keyFactory.generatePublic(X509EncodedKeySpec(publicKeyBytes)) as ECPublicKey
+        val formalPrivateKey = keyFactory.generatePrivate(
+            PKCS8EncodedKeySpec(Base64.decode(keys.privateKey, base64Flag))
+        )
+        val publicKey = keyFactory.generatePublic(
+            X509EncodedKeySpec(Base64.decode(keys.publicKey, base64Flag))
+        )
 
         val keyAgreement = KeyAgreement.getInstance("ECDH")
-        keyAgreement.init(ecPrivateKey)
-        keyAgreement.doPhase(serverPublicKey, true)
+        keyAgreement.init(formalPrivateKey)
+        keyAgreement.doPhase(serverKey, true)
         val sharedSecret = keyAgreement.generateSecret()
 
-        val iv = data.copyOfRange(0, 12)
-        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        val secretKeySpec = SecretKeySpec(sharedSecret, "AES")
-        val parameterSpec = GCMParameterSpec(128, iv) // 128 bit auth tag length
+        val secondSaltInfo = "Content-Encoding: auth\u0000".toByteArray(StandardCharsets.UTF_8)
+        val authKey = Base64.decode(keys.authKey, base64Flag)
+        val secondSalt: ByteArray = deriveKey(authKey, sharedSecret, secondSaltInfo, 32)
+        val keyInfo: ByteArray = info(contentEncoding, publicKey, serverKey)
+        val key = deriveKey(salt, secondSalt, keyInfo, 16)
+        val nonceInfo: ByteArray = info("nonce", publicKey, serverKey)
+        val nonce = deriveKey(salt, secondSalt, nonceInfo, 12)
 
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, parameterSpec)
-        val decryptedBytes = cipher.doFinal(data, 12, data.size - 12)
-        return String(decryptedBytes)
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val aesKey = SecretKeySpec(key, "AES")
+        val iv = GCMParameterSpec(128, nonce)
+        cipher.init(Cipher.DECRYPT_MODE, aesKey, iv)
+        val decrypted = cipher.doFinal(data)
+        return String(decrypted, 2, decrypted.size - 2, StandardCharsets.UTF_8)
     }
 
-    fun String.decodeBase64(): ByteArray =
-        Base64.decode(
+    private fun String.decodeBase64(): ByteArray {
+        return Base64.decode(
             this,
             if (reBase64UrlSafe.containsMatchIn(this)) Base64.URL_SAFE else Base64.DEFAULT,
         )
+    }
+
+    private fun deriveKey(
+        firstSalt: ByteArray,
+        secondSalt: ByteArray,
+        info: ByteArray,
+        length: Int
+    ): ByteArray {
+        val hmacContext = Mac.getInstance("HmacSHA256")
+        hmacContext.init(SecretKeySpec(firstSalt, "HmacSHA256"))
+        val hmac = hmacContext.doFinal(secondSalt)
+        hmacContext.init(SecretKeySpec(hmac, "HmacSHA256"))
+        hmacContext.update(info)
+        val result = hmacContext.doFinal(byteArrayOf(1))
+        return if (result.size <= length) result else Arrays.copyOfRange(result, 0, length)
+    }
+
+    private fun deserializeRawPublicKey(rawBytes: ByteArray): PublicKey? {
+        if (rawBytes.size != 65 && rawBytes.size != 64) return null
+        val kf = KeyFactory.getInstance("EC")
+        val os = ByteArrayOutputStream()
+        os.write(P256_HEAD)
+        if (rawBytes.size == 64) os.write(4)
+        os.write(rawBytes)
+        return kf.generatePublic(X509EncodedKeySpec(os.toByteArray()))
+    }
+
+    private fun info(
+        type: String,
+        clientPublicKey: PublicKey,
+        serverPublicKey: PublicKey
+    ): ByteArray {
+        val info = ByteArrayOutputStream()
+        try {
+            info.write("Content-Encoding: ".toByteArray(StandardCharsets.UTF_8))
+            info.write(type.toByteArray(StandardCharsets.UTF_8))
+            info.write(0)
+            info.write("P-256".toByteArray(StandardCharsets.UTF_8))
+            info.write(0)
+            info.write(0)
+            info.write(65)
+            info.write(serializeRawPublicKey(clientPublicKey))
+            info.write(0)
+            info.write(65)
+            info.write(serializeRawPublicKey(serverPublicKey))
+        } catch (ignore: IOException) {
+        }
+        return info.toByteArray()
+    }
 }
 
 data class CryptoKeys(
     val privateKey: String,
+    val encodedPublicKey: String,
     val publicKey: String,
     val authKey: String,
 )
--- plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/FreadPushService.kt
+++ plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/FreadPushService.kt
@@ -15,20 +15,6 @@ class FreadPushService : FirebaseMessagingService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
 
-    init {
-        Log.d("F_TEST", "init")
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.d("F_TEST", "onCreate")
-    }
-
-    override fun onTimeout(startId: Int) {
-        super.onTimeout(startId)
-        Log.d("F_TEST", "onTimeout: $startId")
-    }
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d("F_TEST", "onNewToken: $token")
@@ -37,32 +23,27 @@ class FreadPushService : FirebaseMessagingService() {
     @OptIn(ExperimentalEncodingApi::class)
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        Log.d("F_TEST", "onMessageReceived: $message")
-        message.data.entries.forEach {
-            Log.d("F_TEST", "onMessageReceived: ${it.key}: ${it.value}")
-        }
         val encodedAccountId = message.data["a"] ?: return
         val messageData = message.data["d"] ?: return
-        val serverPublicKeyEncoded = message.data["Crypto-Key"]?.let { parseDhFromServerKey(it) } ?: return
-        val contentEncoding = message.data["Content-Encoding"]
-        val encryption = message.data["Encryption"]
-
+        val serverPublicKeyEncoded =
+            message.data["Crypto-Key"]?.let { parseDhFromServerKey(it) } ?: return
+        val contentEncoding = message.data["Content-Encoding"] ?: return
+        val encryptionSalt = message.data["Encryption"]?.split("=")?.lastOrNull() ?: return
         val accountId = String(Base64.UrlSafe.decode(encodedAccountId))
-        Log.d("F_TEST", "accountId: $accountId, serverPublicKeyEncoded: $serverPublicKeyEncoded")
-
         coroutineScope.launch {
             val info = getPushRepo().getPushInfo(accountId) ?: return@launch
-            Log.d("F_TEST", "start decrypt data: $messageData")
             val decryptData = try {
                 CryptoUtil.decryptData(
-                    privateKey = info.privateKey,
+                    keys = info,
                     serverPublicKeyEncoded = serverPublicKeyEncoded,
                     data = Base64.decode(messageData),
+                    encryptionSalt = encryptionSalt,
+                    contentEncoding = contentEncoding,
                 )
             } catch (e: Throwable) {
-                Log.d("F_TEST", "decrypted data error: $e")
+                Log.d("F_TEST", "decrypted data error: ${e.stackTraceToString()}")
+                e.printStackTrace()
             }
-            Log.d("F_TEST", "decrypted data: $decryptData")
         }
     }
 

2e28c0574efa782d8950217fcc65763ea9e3f4e3
ZhangKe <kezhang404@gmail.com>
2024-10-22 17:49:33 +0800

    fixed decrypt issues

--- plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/FreadPushService.kt
+++ plugins/activitypub-app/src/androidMain/kotlin/com/zhangke/fread/activitypub/app/internal/push/FreadPushService.kt
@@ -3,8 +3,17 @@ package com.zhangke.fread.activitypub.app.internal.push
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.zhangke.fread.activitypub.app.di.activityPubComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class FreadPushService : FirebaseMessagingService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
 
     init {
         Log.d("F_TEST", "init")
@@ -25,14 +34,48 @@ class FreadPushService : FirebaseMessagingService() {
         Log.d("F_TEST", "onNewToken: $token")
     }
 
+    @OptIn(ExperimentalEncodingApi::class)
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Log.d("F_TEST", "onMessageReceived: $message")
         message.data.entries.forEach {
             Log.d("F_TEST", "onMessageReceived: ${it.key}: ${it.value}")
         }
-        val accountId = message.data["a"]
-        val messageData = message.data["d"]
+        val encodedAccountId = message.data["a"] ?: return
+        val messageData = message.data["d"] ?: return
+        val serverPublicKeyEncoded = message.data["Crypto-Key"]?.let { parseDhFromServerKey(it) } ?: return
+        val contentEncoding = message.data["Content-Encoding"]
+        val encryption = message.data["Encryption"]
 
+        val accountId = String(Base64.UrlSafe.decode(encodedAccountId))
+        Log.d("F_TEST", "accountId: $accountId, serverPublicKeyEncoded: $serverPublicKeyEncoded")
+
+        coroutineScope.launch {
+            val info = getPushRepo().getPushInfo(accountId) ?: return@launch
+            Log.d("F_TEST", "start decrypt data: $messageData")
+            val decryptData = try {
+                CryptoUtil.decryptData(
+                    privateKey = info.privateKey,
+                    serverPublicKeyEncoded = serverPublicKeyEncoded,
+                    data = Base64.decode(messageData),
+                )
+            } catch (e: Throwable) {
+                Log.d("F_TEST", "decrypted data error: $e")
+            }
+            Log.d("F_TEST", "decrypted data: $decryptData")
+        }
+    }
+
+    private fun getPushRepo(): PushInfoRepo {
+        val activityPubComponent = activityPubComponent
+        val database = activityPubComponent.provideActivityPushDatabase(this)
+        return activityPubComponent.providePushInfoRepo(database)
+    }
+
+    private fun parseDhFromServerKey(cryptoKey: String): String? {
+        return cryptoKey.split(";")
+            .firstOrNull()
+            ?.takeIf { it.startsWith("dh=") }
+            ?.removePrefix("dh=")
     }
 }

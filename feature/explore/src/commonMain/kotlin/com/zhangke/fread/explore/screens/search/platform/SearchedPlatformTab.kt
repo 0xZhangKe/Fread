2ba60c792bef075d69eadce5f7f92304481b51cc
zhangke <kezhang404@gmail.com>
2024-11-07 23:25:21 +0800

    show seached content in explore

--- feature/explore/src/commonMain/kotlin/com/zhangke/fread/explore/screens/search/platform/SearchedPlatformTab.kt
+++ feature/explore/src/commonMain/kotlin/com/zhangke/fread/explore/screens/search/platform/SearchedPlatformTab.kt
@@ -1,16 +1,23 @@
 package com.zhangke.fread.explore.screens.search.platform
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getViewModel
-import com.zhangke.framework.composable.ConsumeSnackbarFlow
-import com.zhangke.framework.composable.LocalSnackbarHostState
+import com.zhangke.framework.composable.DefaultLoading
 import com.zhangke.framework.composable.PagerTabOptions
 import com.zhangke.fread.common.page.BasePagerTab
 import com.zhangke.fread.explore.Res
 import com.zhangke.fread.explore.explorer_search_tab_title_server
 import com.zhangke.fread.status.model.IdentityRole
+import com.zhangke.fread.status.ui.source.SearchContentResultUi
 import org.jetbrains.compose.resources.stringResource
 
 class SearchedPlatformTab(private val role: IdentityRole, private val query: String) :
@@ -23,13 +30,29 @@ class SearchedPlatformTab(private val role: IdentityRole, private val query: Str
     @Composable
     override fun TabContent(screen: Screen, nestedScrollConnection: NestedScrollConnection?) {
         super.TabContent(screen, nestedScrollConnection)
-        val viewModel = screen.getViewModel<SearchPlatformViewModel>()
+        val viewModel = with(screen) {
+            getViewModel<SearchPlatformViewModel, SearchPlatformViewModel.Factory> {
+                it.create(role, query)
+            }
+        }
+        val uiState by viewModel.uiState.collectAsState()
+        SearchedSourcesContent(uiState)
+    }
 
-        val snackbarHostState = LocalSnackbarHostState.current
-
-        ConsumeSnackbarFlow(
-            hostState = snackbarHostState,
-            messageTextFlow = viewModel.snackMessageFlow,
-        )
+    @Composable
+    private fun SearchedSourcesContent(
+        uiState: SearchedPlatformUiState
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            if (uiState.searching && uiState.searchedList.isEmpty()) {
+                DefaultLoading()
+            } else {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(uiState.searchedList) {
+                        SearchContentResultUi(content = it, onContentClick = {})
+                    }
+                }
+            }
+        }
     }
 }

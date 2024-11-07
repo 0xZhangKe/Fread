90dd0de5b6b1dd3df20f76fad7dac81732cd94e6
zhangke <kezhang404@gmail.com>
2024-11-07 23:25:21 +0800

    show seached content in explore

--- feature/explore/src/commonMain/kotlin/com/zhangke/fread/explore/screens/search/platform/SearchPlatformViewModel.kt
+++ feature/explore/src/commonMain/kotlin/com/zhangke/fread/explore/screens/search/platform/SearchPlatformViewModel.kt
@@ -1,52 +1,42 @@
 package com.zhangke.fread.explore.screens.search.platform
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.zhangke.framework.composable.TextString
-import com.zhangke.framework.controller.CommonLoadableController
-import com.zhangke.framework.controller.CommonLoadableUiState
 import com.zhangke.framework.ktx.launchInViewModel
+import com.zhangke.fread.common.di.ViewModelFactory
 import com.zhangke.fread.status.StatusProvider
-import com.zhangke.fread.status.platform.BlogPlatform
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
+import com.zhangke.fread.status.model.IdentityRole
+import com.zhangke.fread.status.search.SearchContentResult
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
 
 open class SearchPlatformViewModel @Inject constructor(
     private val statusProvider: StatusProvider,
+    @Assisted private val role: IdentityRole,
+    @Assisted private val query: String,
 ) : ViewModel() {
 
-    private val _snackMessageFlow = MutableSharedFlow<TextString>()
-    val snackMessageFlow: SharedFlow<TextString> get() = _snackMessageFlow
-
-    private val loadableController = CommonLoadableController<BlogPlatform>(
-        viewModelScope,
-        onPostSnackMessage = {
-            launchInViewModel {
-                _snackMessageFlow.emit(it)
-            }
-        },
-    )
-
-    val uiState: StateFlow<CommonLoadableUiState<BlogPlatform>> get() = loadableController.uiState
-
-    fun initQuery(query: String) {
-        if (uiState.value.dataList.isNotEmpty()) return
-        onRefresh(query)
+    fun interface Factory : ViewModelFactory {
+        fun create(role: IdentityRole, query: String): SearchPlatformViewModel
     }
 
-    fun onRefresh(query: String) {
-        loadableController.onRefresh {
-            statusProvider.searchEngine.searchPlatform(query, null)
-        }
-    }
+    private val _uiState = MutableStateFlow(SearchedPlatformUiState.default())
+    val uiState: StateFlow<SearchedPlatformUiState> get() = _uiState
 
-    fun onLoadMore(query: String) {
-        val offset = uiState.value.dataList.size
-        if (offset == 0) return
-        loadableController.onLoadMore {
-            statusProvider.searchEngine.searchPlatform(query, offset)
+    init {
+        launchInViewModel {
+            _uiState.update { it.copy(searching = true) }
+            val list = mutableListOf<SearchContentResult>()
+            statusProvider.searchEngine
+                .searchContent(role, query)
+                .map { it.second }
+                .collect {
+                    list += it
+                }
+            _uiState.update { it.copy(searching = false, searchedList = list) }
         }
     }
 }

4d8891287436190bcf5d8b0e2ce30f2a59999c7e
zhangke <kezhang404@gmail.com>
2024-11-09 22:39:57 +0800

    add search platform in login screen

--- bizframework/status-provider/src/commonMain/kotlin/com/zhangke/fread/status/search/SearchContentResult.kt
+++ bizframework/status-provider/src/commonMain/kotlin/com/zhangke/fread/status/search/SearchContentResult.kt
@@ -1,5 +1,6 @@
 package com.zhangke.fread.status.search
 
+import com.zhangke.fread.status.model.StatusProviderProtocol
 import com.zhangke.fread.status.platform.BlogPlatform
 import com.zhangke.fread.status.platform.PlatformSnapshot
 import com.zhangke.fread.status.source.StatusSource
@@ -11,4 +12,11 @@ sealed interface SearchContentResult {
     data class ActivityPubPlatformSnapshot(val platform: PlatformSnapshot) : SearchContentResult
 
     data class Source(val source: StatusSource) : SearchContentResult
+
+    val protocol: StatusProviderProtocol
+        get() = when (this) {
+            is ActivityPubPlatform -> platform.protocol
+            is ActivityPubPlatformSnapshot -> platform.protocol
+            is Source -> source.protocol
+        }
 }

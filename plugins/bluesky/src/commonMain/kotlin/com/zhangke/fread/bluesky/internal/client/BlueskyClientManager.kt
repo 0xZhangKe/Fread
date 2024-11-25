ebf7567478df7941b281091718a84e5b2dc9aef4
zhangke <kezhang404@gmail.com>
2024-11-25 23:06:43 +0800

    create http engine for BlueSkyClient

--- plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/client/BlueskyClientManager.kt
+++ plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/client/BlueskyClientManager.kt
@@ -1,5 +1,8 @@
 package com.zhangke.fread.bluesky.internal.client
 
+import com.zhangke.framework.architect.http.createHttpClientEngine
+import com.zhangke.framework.architect.json.globalJson
+import com.zhangke.framework.network.FormalBaseUrl
 import com.zhangke.fread.common.di.ApplicationScope
 import com.zhangke.fread.status.model.IdentityRole
 import me.tatarka.inject.annotations.Inject
@@ -9,11 +12,24 @@ class BlueskyClientManager @Inject constructor() {
 
     private val cachedClient = mutableMapOf<IdentityRole, BlueskyClient>()
 
+    private val httpClientEngine by lazy {
+        createHttpClientEngine()
+    }
+
     fun clearCache() {
         cachedClient.clear()
     }
 
     fun getClient(role: IdentityRole): BlueskyClient {
-        return BlueskyClient()
+        cachedClient[role]?.let { return it }
+        return createClient(role.baseUrl!!).also { cachedClient[role] = it }
+    }
+
+    private fun createClient(baseUrl: FormalBaseUrl): BlueskyClient {
+        return BlueskyClient(
+            baseUrl = baseUrl,
+            engine = httpClientEngine,
+            json = globalJson,
+        )
     }
 }

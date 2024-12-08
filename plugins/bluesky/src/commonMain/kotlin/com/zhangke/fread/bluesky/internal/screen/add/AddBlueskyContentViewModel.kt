e645d56e982c03cd531b63621d0e8eb61a1d2b99
zhangke <kezhang404@gmail.com>
2024-12-08 17:42:59 +0800

    calling create-session api success

--- plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/add/AddBlueskyContentViewModel.kt
+++ plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/add/AddBlueskyContentViewModel.kt
@@ -1,11 +1,18 @@
 package com.zhangke.fread.bluesky.internal.screen.add
 
 import androidx.lifecycle.ViewModel
+import com.zhangke.framework.composable.TextString
+import com.zhangke.framework.composable.textOf
+import com.zhangke.framework.ktx.launchInViewModel
 import com.zhangke.framework.network.FormalBaseUrl
 import com.zhangke.fread.bluesky.internal.client.BlueskyClientManager
+import com.zhangke.fread.bluesky.internal.usecase.LoginToBskyUseCase
 import com.zhangke.fread.common.di.ViewModelFactory
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import me.tatarka.inject.annotations.Assisted
@@ -13,6 +20,7 @@ import me.tatarka.inject.annotations.Inject
 
 class AddBlueskyContentViewModel @Inject constructor(
     private val clientManager: BlueskyClientManager,
+    private val loginToBsky: LoginToBskyUseCase,
     @Assisted private val baseUrl: FormalBaseUrl,
 ) : ViewModel() {
 
@@ -30,8 +38,33 @@ class AddBlueskyContentViewModel @Inject constructor(
     )
     val uiState: StateFlow<AddBlueskyContentUiState> = _uiState.asStateFlow()
 
+    private val _snackBarMessage = MutableSharedFlow<TextString>()
+    val snackBarMessage: SharedFlow<TextString> = _snackBarMessage.asSharedFlow()
 
     fun onHostingChange(hosting: String) {
         _uiState.update { it.copy(hosting = hosting) }
+    }
+
+    fun onUserNameChange(username: String){
+        _uiState.update { it.copy(username = username) }
+    }
+
+    fun onPasswordChange(password: String){
+        _uiState.update { it.copy(password = password) }
+    }
+
+    fun onLoginClick() {
+        val hosting = uiState.value.hosting.trim()
+        val username = uiState.value.username.trim()
+        val password = uiState.value.password.trim()
+        launchInViewModel {
+            loginToBsky(hosting, username, password)
+                .onSuccess {
+                    _snackBarMessage.tryEmit(textOf("Login success"))
+                }
+                .onFailure {
+                    _snackBarMessage.tryEmit(textOf("Login failed"))
+                }
+        }
     }
 }

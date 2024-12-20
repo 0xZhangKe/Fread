5c4c29c64f89136e62a662e9b987d9dc542ad053
seiko <seiko_des@outlook.com>
2024-12-20 11:57:01 +0800

    fix merge

--- app-hosting/src/commonMain/kotlin/com/zhangke/fread/screen/main/drawer/MainDrawer.kt
+++ app-hosting/src/commonMain/kotlin/com/zhangke/fread/screen/main/drawer/MainDrawer.kt
@@ -57,8 +57,11 @@ import com.zhangke.fread.common.config.AppCommonConfig
 import com.zhangke.fread.commonbiz.mastodon_logo
 import com.zhangke.fread.feeds.pages.home.EmptyContent
 import com.zhangke.fread.feeds.pages.manager.add.pre.PreAddFeedsScreen
+import com.zhangke.fread.hosting.Res
+import com.zhangke.fread.hosting.main_drawer_donate
 import com.zhangke.fread.hosting.main_drawer_mixed_item_subtitle_1
 import com.zhangke.fread.hosting.main_drawer_mixed_item_subtitle_2
+import com.zhangke.fread.hosting.main_drawer_settings
 import com.zhangke.fread.hosting.main_drawer_title
 import com.zhangke.fread.profile.screen.setting.SettingScreen
 import com.zhangke.fread.status.model.ContentConfig
@@ -219,7 +222,7 @@ private fun MainDrawerContent(
                         contentDescription = "Settings",
                     )
                     Spacer(modifier = Modifier.width(16.dp))
-                    Text(text = stringResource(R.string.main_drawer_settings))
+                    Text(text = stringResource(Res.string.main_drawer_settings))
                 }
                 Row(
                     modifier = Modifier
@@ -236,7 +239,7 @@ private fun MainDrawerContent(
                         contentDescription = "Donate",
                     )
                     Spacer(modifier = Modifier.width(16.dp))
-                    Text(text = stringResource(R.string.main_drawer_donate))
+                    Text(text = stringResource(Res.string.main_drawer_donate))
                 }
             }
         }
@@ -331,8 +334,8 @@ private suspend fun buildSubtitle(
     config: ContentConfig.MixedContent,
 ): AnnotatedString {
     return buildAnnotatedString {
-        val prefix = getString(com.zhangke.fread.hosting.Res.string.main_drawer_mixed_item_subtitle_1)
-        val suffix = getString(com.zhangke.fread.hosting.Res.string.main_drawer_mixed_item_subtitle_2)
+        val prefix = getString(Res.string.main_drawer_mixed_item_subtitle_1)
+        val suffix = getString(Res.string.main_drawer_mixed_item_subtitle_2)
         append(prefix)
         val sizeString = " " + config.sourceUriList.size.toString() + " "
         append(sizeString)

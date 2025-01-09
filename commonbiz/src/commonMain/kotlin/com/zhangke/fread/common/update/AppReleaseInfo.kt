4c078e933bbcaf49f075079b3e468ba723d1860a
ZhangKe <kezhang404@gmail.com>
2025-01-09 16:22:46 +0800

    check app update

--- commonbiz/src/commonMain/kotlin/com/zhangke/fread/common/update/AppReleaseInfo.kt
+++ commonbiz/src/commonMain/kotlin/com/zhangke/fread/common/update/AppReleaseInfo.kt
@@ -1,5 +1,8 @@
 package com.zhangke.fread.common.update
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AppReleaseInfo(
     val versionCode: Long,
     val versionName: String,

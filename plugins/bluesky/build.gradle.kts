d90f2a3e97c0f390a4c8fb66b9a443e2591c8d60
ZhangKe <kezhang404@gmail.com>
2025-01-15 14:08:02 +0800

    refactor globalJson

--- plugins/bluesky/build.gradle.kts
+++ plugins/bluesky/build.gradle.kts
@@ -65,13 +65,14 @@ kotlin {
             }
         }
     }
+    configureCommonMainKsp()
 }
 
 dependencies {
     kspAll(libs.androidx.room.compiler)
     kspAll(libs.kotlinInject.compiler)
     kspAll(libs.auto.service.ksp)
-    kspAll(libs.krouter.collecting.compiler)
+    kspCommonMainMetadata(libs.krouter.collecting.compiler)
 }
 
 
--- plugins/rss/src/commonMain/kotlin/com/zhangke/fread/rss/RssContentManager.kt
+++ plugins/rss/src/commonMain/kotlin/com/zhangke/fread/rss/RssContentManager.kt
@@ -5,7 +5,6 @@ import com.zhangke.fread.status.content.IContentManager
 import com.zhangke.fread.status.model.ContentConfig
 import com.zhangke.fread.status.model.FreadContent
 import com.zhangke.fread.status.platform.BlogPlatform
-import kotlinx.serialization.modules.SerializersModuleBuilder
 import me.tatarka.inject.annotations.Inject
 
 class RssContentManager @Inject constructor() : IContentManager {
@@ -17,6 +16,4 @@ class RssContentManager @Inject constructor() : IContentManager {
     override fun restoreContent(config: ContentConfig): FreadContent? {
         return null
     }
-
-    override fun SerializersModuleBuilder.buildSerializersModule() {}
 }

57efcd976eabd1c4df00f62c594aadf1406ee92c
zhangke <kezhang404@gmail.com>
2025-01-20 11:54:24 +0800

    add BlogEmbed

---
+++ bizframework/status-provider/src/commonMain/kotlin/com/zhangke/fread/status/blog/BlogEmbed.kt
@@ -0,0 +1,21 @@
+package com.zhangke.fread.status.blog
+
+sealed interface BlogEmbed {
+
+    data class Link(
+        val url: String,
+        val title: String,
+        val description: String,
+        val video: Boolean = false,
+        val authorName: String? = null,
+        val authorUrl: String? = null,
+        val providerName: String? = null,
+        val providerUrl: String? = null,
+        val html: String? = null,
+        val width: Int? = null,
+        val height: Int? = null,
+        val image: String? = null,
+        val embedUrl: String? = null,
+        val blurhash: String? = null,
+    )
+}

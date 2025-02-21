fc1321d855a69d8f5fc33c9e201e83b97ed4130c
ZhangKe <kezhang404@gmail.com>
2025-02-21 17:21:16 +0800

    complete publish post screen

--- plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/publish/PublishPostUiState.kt
+++ plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/publish/PublishPostUiState.kt
@@ -3,14 +3,37 @@ package com.zhangke.fread.bluesky.internal.screen.publish
 import androidx.compose.ui.text.input.TextFieldValue
 import app.bsky.graph.ListView
 import com.zhangke.fread.bluesky.internal.account.BlueskyLoggedAccount
-import com.zhangke.fread.status.account.LoggedAccount
+import com.zhangke.fread.bluesky.internal.model.PostInteractionSetting
+import com.zhangke.fread.bluesky.internal.model.ReplySetting
 
 data class PublishPostUiState(
     val content: TextFieldValue,
+    val maxCharacters: Int,
+    val maxMediaCount: Int,
     val account: BlueskyLoggedAccount?,
     val attachment: PublishPostMediaAttachment?,
+    val interactionSetting: PostInteractionSetting,
     val list: List<ListView>,
-)
+) {
+
+    companion object {
+
+        fun default(): PublishPostUiState {
+            return PublishPostUiState(
+                content = TextFieldValue(),
+                maxCharacters = 300,
+                maxMediaCount = 4,
+                interactionSetting = PostInteractionSetting(
+                    allowQuote = true,
+                    replySetting = ReplySetting.Everybody,
+                ),
+                account = null,
+                attachment = null,
+                list = emptyList(),
+            )
+        }
+    }
+}
 
 sealed interface PublishPostMediaAttachment {
 

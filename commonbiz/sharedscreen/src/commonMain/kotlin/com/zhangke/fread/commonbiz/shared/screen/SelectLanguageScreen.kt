c73ea434d9a13cca12cc1d19ef1aba761a486b31
ZhangKe <kezhang404@gmail.com>
2025-02-26 14:41:12 +0800

    finish Bluesky PublishPostScreen UI layer

--- commonbiz/sharedscreen/src/commonMain/kotlin/com/zhangke/fread/commonbiz/shared/screen/SelectLanguageScreen.kt
+++ commonbiz/sharedscreen/src/commonMain/kotlin/com/zhangke/fread/commonbiz/shared/screen/SelectLanguageScreen.kt
@@ -8,10 +8,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,19 +28,27 @@ import com.zhangke.framework.composable.Toolbar
 import com.zhangke.framework.utils.LanguageUtils
 import com.zhangke.framework.utils.Locale
 import com.zhangke.framework.utils.getDisplayName
+import com.zhangke.framework.utils.languageCode
 import com.zhangke.fread.common.page.BaseScreen
 import org.jetbrains.compose.resources.stringResource
 import kotlin.jvm.Transient
 
 class SelectLanguageScreen(
-    @Transient private val onSelected: (Locale) -> Unit,
+    private val selectedLanguages: List<String> = emptyList(),
+    private val multipleSelection: Boolean = false,
+    private val maxSelectCount: Int = 1,
+    @Transient private val onSelected: (List<String>) -> Unit,
 ) : BaseScreen() {
 
     @Composable
     override fun Content() {
         super.Content()
         val languageList = remember {
-            LanguageUtils.getAllLanguages()
+            val list = LanguageUtils.getAllLanguages().map { language ->
+                val selected = selectedLanguages.any { language.languageCode == it }
+                LanguageUiState(language, selected)
+            }
+            mutableStateListOf(*list.toTypedArray())
         }
         val navigator = LocalNavigator.currentOrThrow
         Scaffold(
@@ -42,34 +56,73 @@ class SelectLanguageScreen(
                 Toolbar(
                     title = stringResource(Res.string.shared_select_language_title),
                     onBackClick = navigator::pop,
+                    actions = {
+                        if (multipleSelection) {
+                            IconButton(
+                                onClick = {
+                                    onSelected(languageList.filter { it.selected }
+                                        .map { it.local.languageCode })
+                                    navigator.pop()
+                                },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = "Confirm",
+                                )
+                            }
+                        }
+                    }
                 )
-            }
+            },
         ) { paddingValues ->
             LazyColumn(
                 modifier = Modifier
                     .padding(paddingValues)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
             ) {
-                items(languageList) { language: Locale ->
+                items(languageList) { languageUiState ->
                     Surface(
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(60.dp)
                             .clickable {
-                                onSelected(language)
-                                navigator.pop()
+                                if (!multipleSelection) {
+                                    onSelected(listOf(languageUiState.local.languageCode))
+                                    navigator.pop()
+                                    return@clickable
+                                }
+                                if (languageUiState.selected) {
+                                    languageList[languageList.indexOf(languageUiState)] =
+                                        languageUiState.copy(selected = false)
+                                } else {
+                                    if (languageList.count { it.selected } < maxSelectCount) {
+                                        languageList[languageList.indexOf(languageUiState)] =
+                                            languageUiState.copy(selected = true)
+                                    }
+                                }
                             }
                     ) {
-                        Box(modifier = Modifier.fillMaxSize()) {
+                        Box(
+                            modifier = Modifier.fillMaxSize()
+                                .padding(start = 16.dp, end = 16.dp),
+                        ) {
                             val displayName = remember {
-                                language.getDisplayName(language)
+                                languageUiState.local.getDisplayName(languageUiState.local)
                             }
                             Text(
                                 modifier = Modifier
-                                    .align(Alignment.CenterStart)
-                                    .padding(start = 16.dp),
+                                    .align(Alignment.CenterStart),
                                 text = displayName,
                             )
+                            if (languageUiState.selected) {
+                                Icon(
+                                    modifier = Modifier
+                                        .align(Alignment.CenterEnd),
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = "Checked",
+                                    tint = MaterialTheme.colorScheme.primary,
+                                )
+                            }
                         }
                     }
                 }
@@ -77,3 +130,8 @@ class SelectLanguageScreen(
         }
     }
 }
+
+data class LanguageUiState(
+    val local: Locale,
+    val selected: Boolean,
+)

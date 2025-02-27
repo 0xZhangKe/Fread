7bc76f4e6fc0679719fe900e807fd87555c41481
ZhangKe <kezhang404@gmail.com>
2025-02-27 16:39:09 +0800

    finish publish post

--- framework/src/androidMain/kotlin/com/zhangke/framework/utils/VideoUtils.android.kt
+++ framework/src/androidMain/kotlin/com/zhangke/framework/utils/VideoUtils.android.kt
@@ -1,12 +1,13 @@
 package com.zhangke.framework.utils
 
 import android.media.MediaMetadataRetriever
+import androidx.core.net.toUri
 
 actual class VideoUtils {
 
     actual fun getVideoAspect(uri: String): AspectRatio? {
         val retriever = MediaMetadataRetriever()
-        retriever.setDataSource(uri)
+        retriever.setDataSource(appContext, uri.toUri())
         val width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
             ?.toIntOrNull()
         val height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)

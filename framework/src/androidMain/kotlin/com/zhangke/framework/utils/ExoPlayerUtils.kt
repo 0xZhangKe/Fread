74f78fc902d8a538d43ad85e766e01fc74e3ff26
ZhangKe <kezhang404@gmail.com>
2025-03-03 17:05:22 +0800

    fixed video play error and ui not fit

--- framework/src/androidMain/kotlin/com/zhangke/framework/utils/ExoPlayerUtils.kt
+++ framework/src/androidMain/kotlin/com/zhangke/framework/utils/ExoPlayerUtils.kt
@@ -3,13 +3,12 @@ package com.zhangke.framework.utils
 import android.net.Uri
 import androidx.media3.common.MediaItem
 import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.MediaSource
-import androidx.media3.exoplayer.source.ProgressiveMediaSource
 
 @androidx.media3.common.util.UnstableApi
-fun Uri.toMediaSource(): MediaSource{
+fun Uri.toMediaSource(): MediaSource {
     val defaultDataSourceFactory = DefaultDataSource.Factory(appContext)
     val dataSourceFactory = DefaultDataSource.Factory(appContext, defaultDataSourceFactory)
-    return ProgressiveMediaSource.Factory(dataSourceFactory)
-        .createMediaSource(MediaItem.fromUri(this))
+    return HlsMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(this))
 }

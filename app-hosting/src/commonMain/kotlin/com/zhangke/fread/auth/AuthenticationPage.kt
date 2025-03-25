808a6eb31214675d41a11b240ce30703a3e18677
ZhangKe <kezhang404@gmail.com>
2025-03-25 13:28:49 +0800

    optimize ui

--- app-hosting/src/commonMain/kotlin/com/zhangke/fread/auth/AuthenticationPage.kt
+++ app-hosting/src/commonMain/kotlin/com/zhangke/fread/auth/AuthenticationPage.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
+import androidx.compose.material3.Card
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- framework/src/commonMain/kotlin/com/zhangke/framework/composable/TextWithIcon.kt
+++ framework/src/commonMain/kotlin/com/zhangke/framework/composable/TextWithIcon.kt
@@ -3,7 +3,7 @@ package com.zhangke.framework.composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

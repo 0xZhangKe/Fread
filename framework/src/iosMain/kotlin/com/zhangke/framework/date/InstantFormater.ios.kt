189bf747daaf68d75f38408652c09c487d08f23a
ZhangKe <kezhang404@gmail.com>
2025-03-25 13:28:49 +0800

    optimize ui

--- framework/src/iosMain/kotlin/com/zhangke/framework/date/InstantFormater.ios.kt
+++ framework/src/iosMain/kotlin/com/zhangke/framework/date/InstantFormater.ios.kt
@@ -1,27 +1,30 @@
 package com.zhangke.framework.date
 
 import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
-import kotlinx.datetime.format.DateTimeComponents
 import kotlinx.datetime.format.char
+import kotlinx.datetime.toLocalDateTime
 
 actual class InstantFormater {
 
     actual fun formatToMediumDate(instant: Instant): String {
-        return instant.format(
-            DateTimeComponents.Format {
-                year()
-                char('-')
-                monthNumber()
-                char('-')
-                dayOfMonth()
-                char(' ')
-                hour()
-                char(':')
-                minute()
-                char(':')
-                second()
-            }
-        )
+        return instant.toLocalDateTime(TimeZone.currentSystemDefault())
+            .format(
+                LocalDateTime.Format {
+                    year()
+                    char('-')
+                    monthNumber()
+                    char('-')
+                    dayOfMonth()
+                    char(' ')
+                    hour()
+                    char(':')
+                    minute()
+                    char(':')
+                    second()
+                }
+            )
     }
 }

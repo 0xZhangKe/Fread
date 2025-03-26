22fc2a6b693211d7a2870ce337e32f2e79fc759d
ZhangKe <kezhang404@gmail.com>
2025-03-26 13:37:36 +0800

    add not login label and button

--- plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/feeds/home/HomeFeedsTab.kt
+++ plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/internal/screen/feeds/home/HomeFeedsTab.kt
@@ -1,17 +1,21 @@
 package com.zhangke.fread.bluesky.internal.screen.feeds.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getViewModel
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.zhangke.framework.composable.ConsumeSnackbarFlow
 import com.zhangke.framework.composable.LocalSnackbarHostState
 import com.zhangke.framework.composable.PagerTab
 import com.zhangke.framework.composable.PagerTabOptions
 import com.zhangke.fread.bluesky.internal.model.BlueskyFeeds
+import com.zhangke.fread.bluesky.internal.screen.add.AddBlueskyContentScreen
 import com.zhangke.fread.commonbiz.shared.composable.FeedsContent
 import com.zhangke.fread.status.model.IdentityRole
 
@@ -29,6 +33,7 @@ class HomeFeedsTab(
         screen: Screen,
         nestedScrollConnection: NestedScrollConnection?
     ) {
+        val navigator = LocalNavigator.currentOrThrow
         val viewModel = screen.getViewModel<HomeFeedsContainerViewModel>().getViewModel(feeds, role)
         val uiState by viewModel.uiState.collectAsState()
 
@@ -44,9 +49,16 @@ class HomeFeedsTab(
             nestedScrollConnection = nestedScrollConnection,
             onImmersiveEvent = {},
             onScrollInProgress = {},
+            onLoginClick = {
+                role.baseUrl
+                    ?.let { AddBlueskyContentScreen(baseUrl = it, loginMode = true) }
+                    ?.let { navigator.push(it) }
+            },
         )
 
         val snackbarHostState = LocalSnackbarHostState.current
         ConsumeSnackbarFlow(snackbarHostState, viewModel.errorMessageFlow)
+
+        LaunchedEffect(Unit) { viewModel.onPageResume() }
     }
 }

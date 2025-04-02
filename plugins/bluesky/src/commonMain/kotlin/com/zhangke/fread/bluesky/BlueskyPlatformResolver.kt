84687e5d9e8024a5233d53c6590c970b6ac79080
ZhangKe <kezhang404@gmail.com>
2025-04-02 12:41:42 +0800

    remove code

--- plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/BlueskyPlatformResolver.kt
+++ plugins/bluesky/src/commonMain/kotlin/com/zhangke/fread/bluesky/BlueskyPlatformResolver.kt
@@ -6,7 +6,6 @@ import com.zhangke.fread.status.model.notBluesky
 import com.zhangke.fread.status.platform.BlogPlatform
 import com.zhangke.fread.status.platform.IPlatformResolver
 import com.zhangke.fread.status.platform.PlatformSnapshot
-import com.zhangke.fread.status.uri.FormalUri
 import me.tatarka.inject.annotations.Inject
 
 class BlueskyPlatformResolver @Inject constructor(
@@ -22,10 +21,6 @@ class BlueskyPlatformResolver @Inject constructor(
 
     override suspend fun getSuggestedPlatformSnapshotList(): List<PlatformSnapshot> {
         return blueskyPlatformRepo.getAllPlatform().map { it.toSnapshot() }
-    }
-
-    override suspend fun resolveBySourceUri(sourceUri: FormalUri): Result<BlogPlatform?> {
-        TODO("Not yet implemented")
     }
 
     private fun PlatformSnapshot.toSnapshot(): BlogPlatform? {

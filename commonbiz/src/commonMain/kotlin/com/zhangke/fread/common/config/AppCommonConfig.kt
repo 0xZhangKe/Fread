dc659839d6d1143272eac20fd3c835691c174392
zhangke <kezhang404@gmail.com>
2025-04-26 21:52:08 +0800

    change activity-pub-client dependency to maven

--- commonbiz/src/commonMain/kotlin/com/zhangke/fread/common/config/AppCommonConfig.kt
+++ commonbiz/src/commonMain/kotlin/com/zhangke/fread/common/config/AppCommonConfig.kt
@@ -14,7 +14,7 @@ object AppCommonConfig {
 
     const val PRIVACY_POLICY = "https://fread.xyz/appprivacy.html"
 
-    const val FEEDBACK_URL = "https://github.com/0xZhangKe/FreadIssueTracker/issues/new"
+    const val FEEDBACK_URL = "https://github.com/0xZhangKe/Fread/issues/new"
 
     const val TELEGRAM_GROUP = "https://t.me/+-SlbKcNbJSphNWI1"
 

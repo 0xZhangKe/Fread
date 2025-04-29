4ad1dcb74fa39ad464cf484490c06b7db16f2209
ZhangKe <kezhang404@gmail.com>
2025-04-29 15:44:38 +0800

    move bubble to commonBiz Module

---
+++ commonbiz/src/commonMain/kotlin/com/zhangke/fread/common/bubble/Bubble.kt
@@ -0,0 +1,10 @@
+package com.zhangke.fread.common.bubble
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.runtime.Composable
+
+interface Bubble {
+
+    @Composable
+    fun ColumnScope.Content()
+}

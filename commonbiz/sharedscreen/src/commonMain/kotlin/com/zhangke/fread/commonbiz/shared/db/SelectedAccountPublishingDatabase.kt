c03191eb65661f65d4a905a2d5f7d69dc9fc6b79
ZhangKe <kezhang404@gmail.com>
2025-06-05 17:29:14 +0800

    downgrade kotlin and ksp version

--- commonbiz/sharedscreen/src/commonMain/kotlin/com/zhangke/fread/commonbiz/shared/db/SelectedAccountPublishingDatabase.kt
+++ commonbiz/sharedscreen/src/commonMain/kotlin/com/zhangke/fread/commonbiz/shared/db/SelectedAccountPublishingDatabase.kt
@@ -1,5 +1,6 @@
 package com.zhangke.fread.commonbiz.shared.db
 
+import androidx.room.ConstructedBy
 import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Entity
@@ -7,6 +8,7 @@ import androidx.room.Insert
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 
 private const val TABLE_NAME = "selected_account_publishing"
 
@@ -34,6 +36,7 @@ interface SelectedAccountPublishingDao {
     version = 1,
     exportSchema = false,
 )
+@ConstructedBy(SelectedAccountPublishingDatabaseConstructor::class)
 abstract class SelectedAccountPublishingDatabase : RoomDatabase() {
 
     abstract fun dao(): SelectedAccountPublishingDao
@@ -43,3 +46,11 @@ abstract class SelectedAccountPublishingDatabase : RoomDatabase() {
         const val DB_NAME = "SelectedAccountPublishing.db"
     }
 }
+
+// The Room compiler generates the `actual` implementations.
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object SelectedAccountPublishingDatabaseConstructor :
+    RoomDatabaseConstructor<SelectedAccountPublishingDatabase> {
+    override fun initialize(): SelectedAccountPublishingDatabase
+}
+

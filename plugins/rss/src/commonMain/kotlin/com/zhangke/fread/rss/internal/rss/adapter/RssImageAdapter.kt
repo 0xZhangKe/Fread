77063b69c7441366178aae28a68a41c65e203cb5
ZhangKe <kezhang404@gmail.com>
2025-08-28 16:28:43 +0800

    support RDF format

--- plugins/rss/src/commonMain/kotlin/com/zhangke/fread/rss/internal/rss/adapter/RssImageAdapter.kt
+++ plugins/rss/src/commonMain/kotlin/com/zhangke/fread/rss/internal/rss/adapter/RssImageAdapter.kt
@@ -3,10 +3,14 @@ package com.zhangke.fread.rss.internal.rss.adapter
 import com.prof18.rssparser.model.RssImage
 
 fun RssImage.convert(): com.zhangke.fread.rss.internal.rss.RssImage? {
-    if (!this.isNotEmpty()) return null
+    if (!this.isNotEmptyOrBlank()) return null
     return com.zhangke.fread.rss.internal.rss.RssImage(
         url = this.url!!,
         title = this.title,
         description = this.description,
     )
+}
+
+private fun RssImage.isNotEmptyOrBlank(): Boolean {
+    return !url.isNullOrBlank() || !link.isNullOrBlank()
 }

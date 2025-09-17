985e55834c802d5e978110dbc51d981baff7e851
ZhangKe <kezhang404@gmail.com>
2025-09-17 16:08:02 +0800

    fixed code to Locale error

--- localization/src/androidMain/kotlin/com/zhangke/fread/localization/LanguageCodeUtils.kt
+++ localization/src/androidMain/kotlin/com/zhangke/fread/localization/LanguageCodeUtils.kt
@@ -6,10 +6,10 @@ val LanguageCode.locale: Locale
     get() =
         when (this) {
             LanguageCode.EN_US -> Locale.ENGLISH
-            LanguageCode.DE_DE -> Locale.GERMAN
+            LanguageCode.DE_DE -> Locale("de", "DE")
             LanguageCode.ES_ES -> Locale("es", "ES")
-            LanguageCode.FR_FR -> Locale.FRENCH
-            LanguageCode.JA_JP -> Locale.JAPANESE
+            LanguageCode.FR_FR -> Locale("fr", "FR")
+            LanguageCode.JA_JP -> Locale("ja", "JP")
             LanguageCode.PT_PT -> Locale("pt", "PT")
             LanguageCode.RU_RU -> Locale("ru", "RU")
             LanguageCode.ZH_CN -> Locale.SIMPLIFIED_CHINESE

33d245b33f8570c430e49936a785cde0eca2d5ce
zhangke <kezhang404@gmail.com>
2025-10-08 11:21:16 +0800

    upgrade app to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
 
     defaultConfig {
         applicationId = "com.zhangke.fread"
-        versionCode = 107000
-        versionName = "1.7.0"
+        versionCode = 107010
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

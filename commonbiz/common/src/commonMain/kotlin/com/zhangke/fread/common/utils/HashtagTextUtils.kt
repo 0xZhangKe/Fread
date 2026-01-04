f60634edc0b43b3dbbd1fc140b3f9c43662d4960
Alexthemediocre <65747823+Alexthemediocre@users.noreply.github.com>
2026-01-04 15:50:11 -0500

    Mastodon-style hashtag processing

--- commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/utils/HashtagTextUtils.kt
+++ commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/utils/HashtagTextUtils.kt
@@ -10,6 +10,47 @@ object HashtagTextUtils {
     private val Int.isStartMark: Boolean get() = this == MARK_START
     private val Int.isEndMark: Boolean get() = this == MARK_END
 
+    /**
+     * Returns `true` if this character is a Unicode mark.
+     *
+     * Equivalent to testing if the char would be matched by the regular expression
+     * `\p{M}` or `\p{Mark}` (with Unicode enabled).
+     *
+     * Specifically, a character is a Unicode mark if its [category] is one of
+     * [CharCategory.NON_SPACING_MARK], [CharCategory.COMBINING_SPACING_MARK], and
+     * [CharCategory.ENCLOSING_MARK].
+     */
+    private fun Char.isMark(): Boolean {
+        return when (this.category) {
+            CharCategory.NON_SPACING_MARK,
+            CharCategory.COMBINING_SPACING_MARK,
+            CharCategory.ENCLOSING_MARK
+                -> true
+            else -> false
+        }
+    }
+
+    /**
+     * Returns `true` if this character is a Unicode number.
+     *
+     * Equivalent to testing if the char would be matched by the regular expression
+     * `\p{N}` or `\p{Number}` (with Unicode enabled).
+     *
+     * Specifically, a character is a Unicode number if its [category] is one of
+     * [CharCategory.DECIMAL_DIGIT_NUMBER], [CharCategory.LETTER_NUMBER], and
+     * [CharCategory.OTHER_NUMBER].
+     */
+    private fun Char.isNumber(): Boolean {
+        return when (this.category) {
+            CharCategory.DECIMAL_DIGIT_NUMBER,
+            CharCategory.LETTER_NUMBER,
+            CharCategory.OTHER_NUMBER
+                -> true
+            else -> false
+        }
+    }
+
+
     fun findHashtags(text: String): List<TextRange> {
         if (text.isEmpty()) return emptyList()
         val list = mutableListOf<TextRange>()
@@ -17,34 +58,58 @@ object HashtagTextUtils {
         var index = 0
         var start = MARK_START
         var end = MARK_END
+        var prevIsSep = true
+        var hasAlpha = false
         while (index < text.length) {
             val char = chars[index]
             when {
                 char == '#' -> {
-                    if (start.isStartMark) {
+                    if (prevIsSep) {
                         start = index
-                    } else {
-                        // abc#cde#
+                        hasAlpha = false
+                    } else if (!start.isStartMark) {
                         end = index
                     }
+                    prevIsSep = true
                 }
 
                 char.isWhitespace() -> {
                     if (!start.isStartMark && end.isEndMark) {
                         end = index
                     }
+                    prevIsSep = true
+                }
+
+                char.isLetter() || char.isMark() -> {
+                    prevIsSep = false
+                    hasAlpha = true
+                }
+
+                char.isNumber() || char.category == CharCategory.CONNECTOR_PUNCTUATION -> {
+                    prevIsSep = false
+                }
+
+                char != '\u00b7' && char != '\u200c' -> {
+                    if (!start.isStartMark && end.isEndMark) {
+                        end = index
+                    }
+
+                    if (char == '/' || char == ')') {
+                        prevIsSep = false
+                    }
                 }
             }
             if (!start.isStartMark && !end.isEndMark) {
-                list += TextRange(start = start, end = end)
-                // parse #abc#def as #abc and #def
-                start = if (char == '#') index else MARK_START
+                if (hasAlpha) {
+                    list += TextRange(start = start, end = end)
+                }
+                start = MARK_START
                 end = MARK_END
             }
             index++
         }
 
-        if (index == text.length && !start.isStartMark && end.isEndMark) {
+        if (index == text.length && !start.isStartMark && end.isEndMark && hasAlpha) {
             list += TextRange(start = start, end = index)
         }
         return list
---
+++ commonbiz/common/src/commonTest/kotlin/com/zhangke/fread/common/utils/HashtagTextUtilsTest.kt
@@ -0,0 +1,86 @@
+package com.zhangke.fread.common.utils
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import androidx.compose.ui.text.TextRange
+
+class HashtagTextUtilsTest {
+    @Test
+    fun testMastodonHashtags() {
+        // for Mastodon hashtag behavior see:
+        // https://github.com/mastodon/mastodon/blob/e89acc2302df49cbd7815b031e9c2939632bd204/app/javascript/mastodon/utils/hashtags.ts
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("abc"),
+            listOf()
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#abc"),
+            listOf(
+                TextRange(0, 4),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#abc#def"),
+            listOf(
+                TextRange(0, 4),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#abc #def"), listOf(
+                TextRange(0, 4),
+                TextRange(5, 9),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("##abc"), listOf(
+                TextRange(1, 5),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#a##abc"), listOf(
+                TextRange(0, 2),
+                TextRange(3, 7),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#a!#b_c!##d"), listOf(
+                TextRange(0, 2),
+                TextRange(9, 11),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("foo #"),
+            listOf()
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("foo #_"),
+            listOf()
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("# a"),
+            listOf()
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("(#a)"),
+            listOf(
+                TextRange(1, 3),
+            )
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("()#a"),
+            listOf()
+        )
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("/#a"),
+            listOf()
+        )
+        /*
+        // this will fail. the Mastodon client will process this as a hashtag, but this seems
+        // like fairly obscure or even unintended behavior
+        assertContentEquals(
+            HashtagTextUtils.findHashtags("#___"), listOf(
+                TextRange(0, 4),
+            )
+        )*/
+    }
+}

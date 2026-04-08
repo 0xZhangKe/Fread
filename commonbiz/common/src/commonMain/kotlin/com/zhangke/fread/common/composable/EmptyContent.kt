8ed5df56ed4cad587a9854229b9f95bbfb2085f2
0xZhangKe <kezhang404@gmail.com>
2026-04-08 10:54:57 +0800

    make empty and error content scrollable

--- commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/composable/EmptyContent.kt
+++ commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/composable/EmptyContent.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -47,7 +49,7 @@ fun EmptyContent(
     onClick: (() -> Unit)? = null,
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
--- commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/composable/ErrorContent.kt
+++ commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/composable/ErrorContent.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -37,7 +39,7 @@ fun ErrorContent(
     type: ErrorType = ErrorType.Unknown,
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {

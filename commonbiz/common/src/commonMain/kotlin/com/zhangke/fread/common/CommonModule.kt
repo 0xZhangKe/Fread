78b748b2d36f86730c3f437e09224ca42ed971c2
KonradIT <github@chernowii.com>
2026-05-15 19:37:51 +0200

    automatically detect language written in post using Google's Android built in AI SDK MLKit

--- commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/CommonModule.kt
+++ commonbiz/common/src/commonMain/kotlin/com/zhangke/fread/common/CommonModule.kt
@@ -24,6 +24,7 @@ import com.zhangke.fread.common.review.FreadReviewManager
 import com.zhangke.fread.common.startup.FeedsRepoModuleStartup
 import com.zhangke.fread.common.startup.FreadConfigModuleStartup
 import com.zhangke.fread.common.startup.StartupManager
+import com.zhangke.fread.common.language.LanguageDetector
 import com.zhangke.fread.common.status.StatusIdGenerator
 import com.zhangke.fread.common.status.StatusUpdater
 import com.zhangke.fread.common.status.adapter.ContentConfigAdapter
@@ -52,6 +53,7 @@ val commonModule = module {
     singleOf(::SelectedContentSwitcher)
     singleOf(::StartupManager)
     singleOf(::StatusUpdater)
+    singleOf(::LanguageDetector)
 
     factoryOf(::LinkPreviewCardRepo)
     factoryOf(::AppUpdateManager)
